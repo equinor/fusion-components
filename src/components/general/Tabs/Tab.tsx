7f21b9be3d04dfe3727c7e76733677d5c0170c95
Eskil Lindløv Sand <eslsa@equinor.com>
2019-06-04 08:49:38 +0200

    Tabs scroll (#18)
    * Added on tab click scroll to tabs component
    
    * Refactor story and added focus
    
    * Now uses keyboardnavigation hook to  enter the focused tab
    
    * Removed duplicaition of func declaration
    
    * some adjustments to hover and pressed state
    
    * Moved active tab ref to tabs component

--- src/components/general/Tabs/Tab.tsx
+++ src/components/general/Tabs/Tab.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
 import * as styles from "./styles.less";
 import * as classNames from "classnames";
+import useKeyBoardNavigation from "../../../hooks/useKeyboardNavigation";
 
 type TabProps = {
     isCurrent?: boolean,
     title: string,
     tabKey: string,
     disabled?: boolean,
-    onChange?: () => void,
+    onChange?: (ref: HTMLElement) => void,
     url?: string,
 };
 
-const Tab: React.FC<TabProps> = ({ isCurrent, title, disabled, onChange, url }) => {
+const Tab: React.FC<TabProps> = ({
+    isCurrent,
+    title,
+    disabled,
+    onChange,
+    url,
+}) => {
     const [isPressed, setIsPressed] = React.useState(false);
+
+    const tabRef = useKeyBoardNavigation({
+        onEnter: () => {
+            if (tabRef.current) {
+                !disabled && onChange && onChange(tabRef.current);
+            }
+        },
+    });
 
     const tabClasses = classNames(styles.tab, {
         [styles.current]: isCurrent,
@@ -34,11 +49,13 @@ const Tab: React.FC<TabProps> = ({ isCurrent, title, disabled, onChange, url }) 
     return (
         <a
             className={tabClasses}
-            onClick={() => !disabled && onChange && onChange()}
+            onClick={() => !disabled && onChange && tabRef.current && onChange(tabRef.current)}
             onMouseDown={() => setIsPressed(true)}
             onMouseUp={() => setIsPressed(false)}
-            onMouseLeave={() => isPressed && setIsPressed(false) }
+            onMouseLeave={() => isPressed && setIsPressed(false)}
             href={url}
+            ref={tabRef.setRef}
+            tabIndex={0}
         >
             <div className={titleClasses}>{title}</div>
         </a>
