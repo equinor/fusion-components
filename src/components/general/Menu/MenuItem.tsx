85f065f6f219fe56010d5b74e2acf3d9865a9da5
Martin Førre <martin.forre@bouvet.no>
2019-06-17 11:48:14 +0200

    Initial menu component

---
+++ src/components/general/Menu/MenuItem.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import classNames from "classnames";
+import styles from "./styles.less";
+
+export type MenuItemType = {
+    key: string;
+    title: string;
+    isSelected?: boolean;
+    isDisabled?: boolean;
+};
+
+type MenuItemProps = {
+    item: MenuItemType;
+    index: number;
+    isFocused: boolean;
+    onClick: (e: MenuItemType) => void;
+};
+
+const MenuItem: React.FC<MenuItemProps> = props => {
+    const ref = React.useRef<HTMLButtonElement>(null);
+    const onClick = e => {
+        if (ref.current) {
+            ref.current.blur();
+        }
+
+        props.onClick(props.item);
+        e.preventDefault();
+    };
+
+    const className = classNames({
+        [styles.isSelected]: props.item.isSelected,
+        [styles.isFocused]: props.isFocused,
+    });
+
+    return (
+        <button
+            className={className}
+            disabled={props.item.isDisabled}
+            ref={ref}
+            tabIndex={props.index + 1}
+            onClick={onClick}
+        >
+            <span>{props.item.title}</span>
+        </button>
+    );
+};
+
+export default MenuItem;
---
+++ src/components/general/Menu/index.tsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import classNames from "classnames";
+import useElevationClassName, { Elevation } from "../../../hooks/useElevationClassName";
+import useKeyboardNavigation from "../../../hooks/useKeyboardNavigation";
+import MenuItem, { MenuItemType } from "./MenuItem";
+import styles from "./styles.less";
+
+export type MenuSection = {
+    key: string,
+    title?: string,
+    items: MenuItemType[],
+};
+
+export type MenuProps = {
+    sections: MenuSection[],
+    elevation?: Elevation,
+    onClick?: (item: MenuItemType) => void | Promise<void>,
+    keyboardNavigationRef?: HTMLElement | null,
+};
+
+const Menu: React.FC<MenuProps> = props => {
+    const [focusedSectionKey, setFocusedSectionKey] = React.useState<string | null>(props.sections.length ? props.sections[0].key : null);
+    const [focusedItemKey, setFocusedItemKey] = React.useState<string | null>(props.sections.length && props.sections[0].items.length ? props.sections[0].items[0].key : null);
+
+    const nextOrPrev = (direction: number) => {
+        const sectionIndex = props.sections.findIndex(section => section.key === focusedSectionKey);
+        let nextSectionIndex = sectionIndex;
+
+        if (sectionIndex === -1) {
+            nextSectionIndex = direction === -1 ? props.sections.length - 1 : 0;
+        }
+
+        const items = props.sections[nextSectionIndex].items;
+
+        const itemIndex = items.findIndex(item => item.key === focusedItemKey);
+        let nextItemIndex = itemIndex;
+
+        if (nextItemIndex === -1) {
+            nextSectionIndex = direction === -1 ? props.sections.length - 1 : 0;
+            nextItemIndex = direction === -1 ? props.sections[nextSectionIndex].items.length - 1 : 0;
+        } else if (nextItemIndex === 0 && direction === -1) {
+            nextSectionIndex += (sectionIndex + props.sections.length - 1) % props.sections.length;
+        } else if (nextItemIndex === items.length - 1 && direction === 1) {
+            nextSectionIndex += (sectionIndex + props.sections.length + 1) % props.sections.length;
+        } else {
+            nextItemIndex += direction;
+        }
+
+        setFocusedSectionKey(props.sections[nextSectionIndex].key);
+        setFocusedItemKey(props.sections[nextSectionIndex].items[nextItemIndex].key);
+
+        if(props.sections[nextSectionIndex].items[nextItemIndex].isDisabled) {
+            nextOrPrev(direction);
+        }
+    };
+
+    const reset = () => {
+        setFocusedSectionKey(props.sections.length ? props.sections[0].key : null);
+        setFocusedItemKey(props.sections.length && props.sections[0].items.length ? props.sections[0].items[0].key : null);
+    };
+
+    const onClick = item => {
+        if (props.onClick) {
+            props.onClick(item);
+        }
+    };
+
+    useKeyboardNavigation({
+        onUp: () => nextOrPrev(-1),
+        onDown: () => nextOrPrev(+1),
+        onEnter: () => {
+            const section = props.sections.find(section => section.key === focusedSectionKey);
+            if (!section) {
+                return;
+            }
+
+            const item = section.items.find(item => item.key === focusedItemKey);
+            if (!item) {
+                return;
+            }
+
+            onClick(item);
+        },
+        onEscape: reset,
+    }, props.keyboardNavigationRef);
+
+    React.useEffect(reset, [props.sections]);
+
+    const className = classNames(styles.container, useElevationClassName(props.elevation));
+
+    return (
+        <div className={className}>
+            {props.sections.map(section => (
+                <section key={section.key}>
+                    {section.items.map((item, index) => (
+                        <MenuItem
+                            key={item.key}
+                            index={index}
+                            isFocused={
+                                focusedSectionKey === section.key && focusedItemKey === item.key
+                            }
+                            item={item}
+                            onClick={onClick}
+                        />
+                    ))}
+                </section>
+            ))}
+        </div>
+    );
+};
+
+Menu.defaultProps = {
+    sections: [],
+    elevation: 2,
+};
+
+export default Menu;
