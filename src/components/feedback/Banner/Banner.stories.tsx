f2ce00c21564b3c0cb4bbf2bb89c7dd4da15f033
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2019-06-18 13:17:52 +0200

    refactor

--- src/components/feedback/Banner/Banner.stories.tsx
+++ src/components/feedback/Banner/Banner.stories.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Banner from './index';
-import Button from '../../general/Button';
+import {ErrorIcon} from "../../icons/components/alert"
 
 const BannerStory = () => {
+
     return (
         <div style={{display:"flex", justifyContent:"center"}}>
 
         <Banner
             message="This is the banner message, loooogn text, This is the banner message, loooogn textThis is the banner message, loooogn textThis is the banner message, loooogn textv"
-            actions={[
-                <Button frameless comfortable key="do-something">
-                    Do something
-                </Button>,
-                <Button frameless comfortable key="dismiss">
-                    Dismiss
-                </Button>,
-            ]}
+            cancelLabel = "Dismiss"
+            onCancel = {() => console.log("cancel")}
+            action
+            actionLabel = "Do something"
+            onAction = {() => console.log("did something")}
+            icon= {<ErrorIcon outline/>}
         />
         </div>
 
--- src/components/feedback/Banner/index.tsx
+++ src/components/feedback/Banner/index.tsx
@@ -1,24 +1,47 @@
 import * as React from 'react';
 import styles from './styles.less';
+import Button from '../../general/Button';
 
 type BannerProps = {
     message: string;
-    actions: Array<JSX.Element>;
-    icon?: any
+    icon?: any;
+    cancelLabel: string;
+    onCancel: () => void;
+    action: boolean;
+    actionLabel: string;
+    onAction: () => void;
 };
 
-const Banner: React.FC<BannerProps> = ({ message, actions }) => {
-    const bannerActions = React.useMemo(() => {
-        if (!actions || !actions.length) {
-            return null
-        }
-        return <div className={styles.actions}>{actions.map(action => action)}</div>;;
-    }, [actions]);
+const Banner: React.FC<BannerProps> = ({
+    message,
+    icon,
+    cancelLabel,
+    onCancel,
+    action,
+    actionLabel,
+    onAction,
+}) => {
+    const cancelButton = (
+        <Button frameless comfortable onClick={onCancel}>
+            {cancelLabel}
+        </Button>
+    );
+    const actionButton = action ? (
+        <Button frameless comfortable onClick={onAction}>
+            {actionLabel}
+        </Button>
+    ) : null;
+
+    const bannerIcon = icon ? <div className={styles.icon}>{icon}</div> : null;
 
     return (
         <div className={styles.container}>
+            {}
             <div className={styles.message}>{message}</div>
-            {bannerActions}
+            <div className={styles.actions}>
+                {actionButton}
+                {cancelButton}
+            </div>
         </div>
     );
 };
