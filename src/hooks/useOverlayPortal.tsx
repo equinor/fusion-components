cf33abba65bc128d73a5ec4be649784017b812eb
Martin Førre <martin.forre@bouvet.no>
2019-06-21 12:52:02 +0200

    Forward fusion context to overlay portal and use the overlay ref instead of dom node

--- src/hooks/useOverlayPortal.tsx
+++ src/hooks/useOverlayPortal.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { ReactNode, useRef, useEffect } from 'react';
 import * as ReactDOM from 'react-dom';
+import { FusionContext, useFusionContext } from '@equinor/fusion';
 import useOverlayContainer from './useOverlayContainer';
 
 export default (isVisible: Boolean, content: ReactNode): void => {
+    const fusionContext = useFusionContext();
     const ref = useRef(document.createElement('div'));
     const overlayContainer = useOverlayContainer();
 
@@ -12,7 +14,10 @@ export default (isVisible: Boolean, content: ReactNode): void => {
             return;
         }
 
-        ReactDOM.render(<React.Fragment>{content}</React.Fragment>, ref.current);
+        ReactDOM.render(
+            <FusionContext.Provider value={fusionContext}>{content}</FusionContext.Provider>,
+            ref.current
+        );
         overlayContainer.prepend(ref.current);
 
         return () => {
