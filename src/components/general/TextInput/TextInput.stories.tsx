6808d247656b7bfbfdc4302263cbe11ef540885d
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2019-06-24 13:26:53 +0200

    added disabled and errormessage

--- src/components/general/TextInput/TextInput.stories.tsx
+++ src/components/general/TextInput/TextInput.stories.tsx
@@ -1,20 +1,34 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import TextInput from "./index";
-import {WarningIcon} from "../../icons/components/alert"
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import TextInput from './index';
+import { WarningIcon } from '../../icons/components/alert';
 
-const TextInputStory =() => {
-    const [value, setValue] = React.useState("");
-    return(
+const TextInputStory = () => {
+    const [value, setValue] = React.useState('');
+    return (
         <React.Fragment>
-        <TextInput onChange={value =>setValue(value) } value={value} label="Text Input" placeholder="Input text"/>
-        <br />
-        <TextInput error label="Error input" placeholder="Error" onChange={value =>setValue(value) } value={value}  icon={<WarningIcon outline cursor="default"/>}/>
-        <br />
-        <TextInput  onChange={value =>setValue(value) } value={value} />
+            <TextInput
+                onChange={value => setValue(value)}
+                value={value}
+                label="Text Input"
+                placeholder="Input text"
+                isRequired
+                disabled
+            />
+            <br />
+            <TextInput
+                error
+                label="Error input"
+                placeholder="Error"
+                errorMessage="An error occured"
+                onChange={value => setValue(value)}
+                value={value}
+                icon={<WarningIcon outline cursor="default" color="#FF3B3B" />}
+            />
+            <br />
+            <TextInput onChange={value => setValue(value)} value={value} disabled/>
         </React.Fragment>
-        
-    )
-}
+    );
+};
 
-storiesOf("General|TextInput", module).add("Default", () => <TextInputStory />);
+storiesOf('General|TextInput', module).add('Default', () => <TextInputStory />);
--- src/components/general/TextInput/index.tsx
+++ src/components/general/TextInput/index.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 type TextInputProps = {
     disabled?: boolean;
     error?: boolean;
-    helperText?: string;
+    errorMessage?: string;
     isRequired?: boolean;
     placeholder?: string;
     label?: string;
@@ -15,68 +15,102 @@ type TextInputProps = {
     onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 };
 
-const TextInput: React.FC<TextInputProps> = ({
-    disabled = false,
-    error,
-    helperText,
-    isRequired = false,
-    placeholder = '',
-    label,
-    onChange,
-    value = '',
-    icon,
-    onBlur,
-    ...rest
-}) => {
-    const inputRef = React.useRef<HTMLInputElement>(null);
-    const [focus, setFocus] = React.useState(false);
+const TextInput = React.forwardRef<HTMLDivElement | null, React.PropsWithChildren<TextInputProps>>(
+    (
+        {
+            disabled = false,
+            error,
+            errorMessage,
+            isRequired = false,
+            placeholder = '',
+            label,
+            onChange,
+            value = '',
+            icon,
+            onBlur,
+            ...props
+        },
+        ref
+    ) => {
+        const inputRef = React.useRef<HTMLInputElement>(null);
+        const [focus, setFocus] = React.useState(false);
+        const inputContainerRef = ref as React.RefObject<HTMLDivElement | null>;
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (!disabled) {
-            const newValue = event.target.value;
-            onChange(newValue);
-        }
-    };
+        const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            if (!disabled) {
+                const newValue = event.target.value;
+                onChange(newValue);
+            }
+        };
 
-    const setInputFocus = () => {
-        if (inputRef.current && !disabled) {
-            inputRef.current.focus();
-            setFocus(true);
-        }
-    };
-    const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-        if (!disabled) {
-            setFocus(false);
-            onBlur && onBlur(event);
-        }
-    };
+        const setInputFocus = () => {
+            if (inputRef.current && !disabled) {
+                inputRef.current.focus();
+                setFocus(true);
+            }
+        };
+        const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+            if (!disabled) {
+                setFocus(false);
+                onBlur && onBlur(event);
+            }
+        };
 
-    const inputContentClasses = classNames(styles.inputContent, {
-        [styles.focus]: focus,
-        [styles.error]: error,
-    });
+        const inputLabel = label && (
+            <span className={styles.label}>
+                {label}
+                {isRequired && '*'}
+            </span>
+        );
 
-    return (
-        <div className={styles.inputContainer} >
-            
-            <div className={inputContentClasses} onClick={setInputFocus}>
-                <div className={styles.inputTextContent}>
-                    <span className={styles.label}>{label}</span>
-                    <input
-                    ref={inputRef}
-                    placeholder={placeholder}
-                    onBlur={handleBlur}
-                    value={value}
-                    onChange={handleChange}
-                    {...rest}
-                />
+        const inputIcon = icon && <div className={styles.icon}>{icon}</div>;
+
+        const inputHelperText = React.useMemo(() => {
+            if (errorMessage && error) {
+                return (
+                    <div className={classNames(styles.helperText, styles.error)}>
+                        {errorMessage}
+                    </div>
+                );
+            }
+            if (isRequired) {
+                return <div className={styles.helperText}>*Required</div>;
+            }
+            return null;
+        }, [errorMessage, error, isRequired]);
+
+        const inputContentClasses = classNames(styles.inputContent, {
+            [styles.focus]: focus,
+            [styles.error]: error,
+            [styles.disabled]: disabled,
+        });
+
+        const inputTextContentClasses = classNames(styles.inputTextContent, {
+            [styles.labelLess]: !label,
+        });
+
+        return (
+            <div className={styles.inputContainer} ref={inputContainerRef}>
+                <div className={inputContentClasses} onClick={setInputFocus}>
+                    <div className={inputTextContentClasses}>
+                        {inputLabel}
+                        <input
+                            ref={inputRef}
+                            placeholder={placeholder}
+                            onBlur={handleBlur}
+                            value={!disabled ? value : ''}
+                            onChange={handleChange}
+                            disabled={disabled}
+                            {...props}
+                        />
+                    </div>
+                    {inputIcon}
                 </div>
-                <div className={styles.icon}>{icon}</div>
+                {inputHelperText}
             </div>
-            <div className={styles.helperText}>{helperText}</div>
-        </div>
-    );
-};
+        );
+    }
+);
 
 TextInput.displayName = 'TextInput';
 
