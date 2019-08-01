d25c1a78d6afc4cba667006a0a48ab164dea40ac
Martin Førre <martin.forre@bouvet.no>
2019-08-01 15:27:30 +0200

    Update docs

--- docs/vendors~main.ca9f30bbe003f5999744.bundle.js
+++ docs/vendors~main.ca9f30bbe003f5999744.bundle.js
@@ -102,4 +102,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.bd6e4a4672c3973fa077.bundle.js.map
+//# sourceMappingURL=vendors~main.ca9f30bbe003f5999744.bundle.js.map
