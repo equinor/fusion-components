9b6642dbe9f6d6b1681da78e40c3cf20a4a2c373
Martin Førre <martin.forre@bouvet.no>
2019-08-09 15:30:19 +0200

    Add missing files

---
+++ src/components/general/Calendar/Day.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import classNames from 'classnames';
+import { CalendarDate, isSameDate } from '@equinor/fusion';
+
+import styles from './styles.less';
+import { IconButton } from '@equinor/fusion-components';
+
+type DayProps = {
+    date: CalendarDate;
+    interactive?: boolean;
+    selectedDate?: Date;
+    onClick?: (date: Date) => void;
+};
+
+const Day: React.FC<DayProps> = ({ date, interactive, onClick, selectedDate }) => {
+    const dayClassNames = classNames(styles.day, {
+        [styles.isToday]: date.isToday,
+        [styles.notSelectedMonth]: !date.isSelectedMonth,
+    });
+
+    const isSelectedDate = selectedDate && isSameDate(selectedDate, date.date);
+
+    return (
+        <span className={dayClassNames}>
+            {interactive ? (
+                <IconButton
+                    active={isSelectedDate}
+                    disabled={!date.isSelectedMonth}
+                    toggler={date.isToday && !isSelectedDate}
+                    onClick={() => onClick && onClick(date.date)}
+                >
+                    {date.value}
+                </IconButton>
+            ) : (
+                date.value
+            )}
+        </span>
+    );
+};
+
+export default Day;
