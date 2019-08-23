7b3fa787c6d59cc5f5683ab63b5e357ba3e524c3
Morten Salte <morten.salte@bouvet.no>
2019-08-23 14:00:43 +0200

    First attempt at making a PersonPhoto component

--- src/components/general/PersonPhoto/__stories__/PersonPhoto.stories.tsx
+++ src/components/general/PersonPhoto/__stories__/PersonPhoto.stories.tsx
@@ -6,8 +6,35 @@ import PersonPhoto from '../index';
 storiesOf('General|PersonPhoto', module)
     .addParameters({ jest: ['PersonPhoto.stories.jsx'] })
     .addDecorator(withFusionStory('PersonPhoto'))
-    .add('Default', () => (
-        <div style={{ padding: 8 }}>
-            <PersonPhoto />
-        </div>
+    .add('Affiliate (XLarge)', () => (
+        <PersonPhoto
+            title="Morten Salte"
+            personId="e92c631b-94ae-4670-8f1e-60cdc2122edc"
+            affiliation="affiliate"
+            size="xlarge"
+        />
+    ))
+    .add('Consultant (Large)', () => (
+        <PersonPhoto
+            title="Morten Salte"
+            personId="e92c631b-94ae-4670-8f1e-60cdc2122edc"
+            affiliation="consultant"
+            size="large"
+        />
+    ))
+    .add('External Hire (Medium)', () => (
+        <PersonPhoto
+            title="Morten Salte"
+            personId="e92c631b-94ae-4670-8f1e-60cdc2122edc"
+            affiliation="externalhire"
+            size="medium"
+        />
+    ))
+    .add('Consultant (Small)', () => (
+        <PersonPhoto
+            title="Morten Salte"
+            personId="e92c631b-94ae-4670-8f1e-60cdc2122edc"
+            affiliation="consultant"
+            size="small"
+        />
     ));
--- src/components/general/PersonPhoto/index.tsx
+++ src/components/general/PersonPhoto/index.tsx
@@ -1,7 +1,99 @@
 import React from 'react';
 import styles from './styles.less';
 import classNames from 'classnames';
+import { useFusionContext, useComponentDisplayClassNames } from '@equinor/fusion';
+import ConsultantIcon from './icons/ConsultantIcon';
+import ExternalHireIcon from './icons/ExternalHireIcon';
+import AffiliateIcon from './icons/AffiliateIcon';
 
-export default () => {
-    return <div className={classNames(styles.container)}>Person photo!</div>;
+export type PhotoSize = 'xlarge' | 'large' | 'medium' | 'small';
+export type Affiliation = 'externalhire' | 'consultant' | 'affiliate';
+
+export type PersonPhotoProps = {
+    personId: string;
+    affiliation: Affiliation;
+    size: PhotoSize;
+    title?: string;
+};
+
+const friendlyAffiliationName = (affiliation: string) => {
+    switch (affiliation) {
+        case 'externalhire':
+            return 'External Hire';
+        case 'consultant':
+            return 'Consultant';
+        case 'affiliate':
+            return 'Affiliate';
+    }
+};
+
+const iconSizes = {
+    xlarge: { width: 24, height: 24 },
+    large: { width: 16, height: 16 },
+    medium: { width: 16, height: 16 },
+    small: { width: 12, height: 12 },
+};
+
+export default ({ personId, affiliation, size, title }: PersonPhotoProps) => {
+    const fusionContext = useFusionContext();
+    const urlToPhoto = fusionContext.http.resourceCollections.people.getPersonPhoto(personId);
+
+    const photoClassNames = classNames(
+        styles.photoContainer,
+        useComponentDisplayClassNames(styles),
+        {
+            [styles.xlarge]: size === 'xlarge',
+            [styles.large]: size === 'large',
+            [styles.medium]: size === 'medium',
+            [styles.small]: size === 'small',
+        }
+    );
+
+    const iconClassNames = classNames(
+        styles.affiliationContainer,
+        useComponentDisplayClassNames(styles),
+        {
+            [styles.xlarge]: size === 'xlarge',
+            [styles.large]: size === 'large',
+            [styles.medium]: size === 'medium',
+            [styles.small]: size === 'small',
+            [styles.externalHire]: affiliation === 'externalhire',
+            [styles.consultant]: affiliation === 'consultant',
+            [styles.affiliate]: affiliation === 'affiliate',
+        }
+    );
+
+    return (
+        <React.Fragment>
+            <img className={photoClassNames} src="https://via.placeholder.com/150" />
+            <div
+                title={title}
+                className={photoClassNames}
+                style={{
+                    backgroundImage: `url('${urlToPhoto}')`,
+                }}
+            >
+                <div className={iconClassNames} title={friendlyAffiliationName(affiliation)}>
+                    {affiliation === 'consultant' && (
+                        <ConsultantIcon
+                            width={iconSizes[size].width}
+                            height={iconSizes[size].height}
+                        />
+                    )}
+                    {affiliation === 'externalhire' && (
+                        <ExternalHireIcon
+                            width={iconSizes[size].width}
+                            height={iconSizes[size].height}
+                        />
+                    )}
+                    {affiliation === 'affiliate' && (
+                        <AffiliateIcon
+                            width={iconSizes[size].width}
+                            height={iconSizes[size].height}
+                        />
+                    )}
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
