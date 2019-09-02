0c8cc2f07f5ef88607a7322f4c057461ed948db3
Martin Førre <martin.forre@bouvet.no>
2019-09-02 14:46:16 +0200

    Add onExpand to position card

--- src/components/org/PositionCard/PositionCard.stories.tsx
+++ src/components/org/PositionCard/PositionCard.stories.tsx
@@ -18,7 +18,7 @@ const position: Position = {
     externalId: '800',
     instances: [
         {
-            appliesFrom: new Date(),
+            appliesFrom: new Date(new Date().getFullYear() - 1, 0),
             appliesTo: new Date(new Date().getFullYear() + 1, 0),
             location: {
                 code: '213',
@@ -55,83 +55,6 @@ const position: Position = {
     parentPositionId: '0',
 };
 
-const positionWithMultipleInstances: Position = {
-    ...position,
-    name: 'Piping Engineer',
-    instances: [
-        ...position.instances,
-        {
-            appliesFrom: new Date(),
-            appliesTo: new Date(new Date().getFullYear(), new Date().getMonth() + 4),
-            location: {
-                code: '616',
-                country: 'Norway',
-                id: '161216',
-                name: 'Oslo',
-            },
-            obs: 'obs',
-            percent: 100,
-            type: 'type',
-            assignedPerson: {
-                accountType: 'Consultant',
-                azureUniqueId: 'e92c631b-94ae-4670-8f1e-60cdc2122edc',
-                company: {
-                    id: '8/4150ega',
-                    name: 'Bouvet',
-                },
-                contracts: [],
-                positions: [],
-                roles: [],
-                upn: 'msal@equinor.com',
-                department: 'department',
-                name: 'Morten Salte',
-                jobTitle: 'Job title',
-                mail: 'msal@equinor.com',
-                mobilePhone: '12345678',
-                officeLocation: 'Stavanger',
-            },
-            properties: {},
-        },
-        {
-            appliesFrom: new Date(),
-            appliesTo: new Date(new Date().getFullYear() + 2, 0),
-            location: {
-                code: '113',
-                country: 'Norway',
-                id: '11331',
-                name: 'Bergen',
-            },
-            obs: 'obs',
-            percent: 100,
-            type: 'type',
-            assignedPerson: {
-                accountType: 'External',
-                azureUniqueId: '3tgweafgw<fwedgw',
-                company: {
-                    id: '984018gwsedgw3gh',
-                    name: 'External Company',
-                },
-                contracts: [],
-                positions: [],
-                roles: [],
-                upn: 'jiji@gmail.com',
-                department: 'department',
-                name: 'Jim Jimsen',
-                jobTitle: 'Job title',
-                mail: 'jiji@gmail.com',
-                mobilePhone: '12345678',
-                officeLocation: 'Stavanger',
-            },
-            properties: {},
-        },
-    ],
-};
-
-const positions = {
-    'Standard position': position,
-    'Multiple assignments': positionWithMultipleInstances,
-};
-
 const InteractiveStory = () => {
     const [isSelected, setIsSelected] = useState(false);
 
@@ -143,11 +66,13 @@ const InteractiveStory = () => {
     return (
         <PositionCard
             onClick={toggleSelected}
-            position={select('Position', positions, position)}
+            position={position}
+            instance={position.instances[0]}
             showDate={boolean('Show date', true)}
             showExternalId={boolean('Show Pims id', true)}
             showLocation={boolean('Show location', true)}
             isSelected={isSelected}
+            onExpand={action('onExpand')}
         />
     );
 };
@@ -157,7 +82,8 @@ storiesOf('Pro org|Position Card', module)
     .addDecorator(withFusionStory('Pro org position card'))
     .add('Default', () => (
         <PositionCard
-            position={select('Position', positions, position)}
+            position={position}
+            instance={position.instances[0]}
             showDate={boolean('Show date', true)}
             showExternalId={boolean('Show Pims id', true)}
             showLocation={boolean('Show location', true)}
@@ -165,3 +91,75 @@ storiesOf('Pro org|Position Card', module)
         />
     ))
     .add('Interactive', () => <InteractiveStory />);
+
+// const positionWithMultipleInstances: Position = {
+//     ...position,
+//     name: 'Piping Engineer',
+//     instances: [
+//         ...position.instances,
+//         {
+//             appliesFrom: new Date(),
+//             appliesTo: new Date(new Date().getFullYear(), new Date().getMonth() + 4),
+//             location: {
+//                 code: '616',
+//                 country: 'Norway',
+//                 id: '161216',
+//                 name: 'Oslo',
+//             },
+//             obs: 'obs',
+//             percent: 100,
+//             type: 'type',
+//             assignedPerson: {
+//                 accountType: 'Consultant',
+//                 azureUniqueId: 'e92c631b-94ae-4670-8f1e-60cdc2122edc',
+//                 company: {
+//                     id: '8/4150ega',
+//                     name: 'Bouvet',
+//                 },
+//                 contracts: [],
+//                 positions: [],
+//                 roles: [],
+//                 upn: 'msal@equinor.com',
+//                 department: 'department',
+//                 name: 'Morten Salte',
+//                 jobTitle: 'Job title',
+//                 mail: 'msal@equinor.com',
+//                 mobilePhone: '12345678',
+//                 officeLocation: 'Stavanger',
+//             },
+//             properties: {},
+//         },
+//         {
+//             appliesFrom: new Date(),
+//             appliesTo: new Date(new Date().getFullYear() + 2, 0),
+//             location: {
+//                 code: '113',
+//                 country: 'Norway',
+//                 id: '11331',
+//                 name: 'Bergen',
+//             },
+//             obs: 'obs',
+//             percent: 100,
+//             type: 'type',
+//             assignedPerson: {
+//                 accountType: 'External',
+//                 azureUniqueId: '3tgweafgw<fwedgw',
+//                 company: {
+//                     id: '984018gwsedgw3gh',
+//                     name: 'External Company',
+//                 },
+//                 contracts: [],
+//                 positions: [],
+//                 roles: [],
+//                 upn: 'jiji@gmail.com',
+//                 department: 'department',
+//                 name: 'Jim Jimsen',
+//                 jobTitle: 'Job title',
+//                 mail: 'jiji@gmail.com',
+//                 mobilePhone: '12345678',
+//                 officeLocation: 'Stavanger',
+//             },
+//             properties: {},
+//         },
+//     ],
+// };
--- src/components/org/PositionCard/index.tsx
+++ src/components/org/PositionCard/index.tsx
@@ -1,6 +1,11 @@
 import React, { useCallback, useMemo } from 'react';
 import classNames from 'classnames';
-import { Position, useComponentDisplayClassNames, PositionInstance } from '@equinor/fusion';
+import {
+    Position,
+    useComponentDisplayClassNames,
+    PositionInstance,
+    useComponentDisplayType,
+} from '@equinor/fusion';
 
 import styles from './styles.less';
 import PositionPhoto from './components/PositionPhoto';
@@ -8,34 +13,25 @@ import PositionInstanceComponent from './components/PositionInstance';
 
 type PositionCardProps = {
     position: Position;
+    instance: PositionInstance;
     isSelected?: boolean;
     showExternalId: boolean;
     showLocation: boolean;
     showDate: boolean;
     onClick?: (position: Position, instance: PositionInstance) => void;
-};
-
-const getCurrentInstance = (position: Position) => {
-    const now = new Date().getTime();
-    return (
-        position.instances.find(instance => {
-            const from = new Date(instance.appliesFrom).getTime();
-            const to = new Date(instance.appliesTo).getTime();
-            return now >= from && to >= now;
-        }) || position.instances[0]
-    );
+    onExpand?: (position: Position, instance: PositionInstance) => void;
 };
 
 const PositionCard: React.FC<PositionCardProps> = ({
     position,
+    instance,
     isSelected,
     showExternalId,
     showLocation,
     showDate,
     onClick,
+    onExpand,
 }) => {
-    const currentInstance = useMemo(() => getCurrentInstance(position), [position]);
-
     const containerClassNames = classNames(
         styles.context,
         styles.container,
@@ -49,24 +45,23 @@ const PositionCard: React.FC<PositionCardProps> = ({
 
     const onClickHandler = useCallback(() => {
         if (onClick) {
-            onClick(position, currentInstance);
+            onClick(position, instance);
         }
-    }, [position, currentInstance, onClick]);
+    }, [position, instance, onClick]);
+
+    const componentDisplayType = useComponentDisplayType();
 
     return (
         <div className={containerClassNames} onClick={onClickHandler}>
-            <PositionPhoto
-                position={position}
-                currentInstance={currentInstance}
-                onClick={onClick}
-            />
+            <PositionPhoto position={position} currentInstance={instance} onClick={onClick} />
             <PositionInstanceComponent
                 position={position}
-                instance={currentInstance}
-                showLocation={showLocation}
-                showDate={showDate}
+                instance={instance}
+                showLocation={showLocation && componentDisplayType !== 'Compact'}
+                showDate={showDate && componentDisplayType !== 'Compact'}
                 showExternalId={showExternalId}
                 onClick={onClick}
+                onExpand={onExpand}
             />
         </div>
     );
