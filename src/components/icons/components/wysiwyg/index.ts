5ee5088f435aee0f33000c8f3c9afa17c7113a13
Hallgeir Gutvik <hallgeir.gutvik@bouvet.no>
2019-11-11 09:32:55 +0100

    Sort exports. Add export for new icon components

--- src/components/icons/components/wysiwyg/index.ts
+++ src/components/icons/components/wysiwyg/index.ts
@@ -1,4 +1,6 @@
+export { default as FormatBoldIcon } from './FormatBoldIcon';
+export { default as FormatBulletListIcon } from './FormatBulletListIcon';
+export { default as FormatItalicsIcon } from './FormatItalicsIcon';
+export { default as FormatNumberedListIcon } from './FormatNumberedListIcon';
 export { default as LinkIcon } from './LinkIcon';
 export { default as TitleIcon } from './TitleIcon';
-export { default as FormatItalicsIcon } from './FormatItalicsIcon';
-export { default as FormatBoldIcon } from './FormatBoldIcon';
