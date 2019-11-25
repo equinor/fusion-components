2738d42f9ea92f346ce78d162797cbff08a74e10
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2019-11-25 08:59:32 +0100

    Build storybook

--- docs/vendors~main.15927756638dbb4c810a.bundle.js
+++ docs/vendors~main.15927756638dbb4c810a.bundle.js
@@ -104,4 +104,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.b8aedfbc13f44676f204.bundle.js.map
+//# sourceMappingURL=vendors~main.15927756638dbb4c810a.bundle.js.map
