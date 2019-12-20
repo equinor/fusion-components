ef3bafd49103772010c11082bb9ec2c0db7a8715
Ingrid Klepsvik <ingrid.klepsvik@olavstoppen.no>
2019-12-20 12:08:50 +0100

    destructed props in skeleton bar

--- src/components/feedback/Skeleton/Bar.tsx
+++ src/components/feedback/Skeleton/Bar.tsx
@@ -8,15 +8,15 @@ type SkeletonBarProps = {
     height?: number | string;
 };
 
-const SkeletonBar: React.FC<SkeletonBarProps> = ({ height, ...props }) => {
+const SkeletonBar: React.FC<SkeletonBarProps> = ({ height, width }) => {
     const displayClassNames = useComponentDisplayClassNames(styles);
     const barClassNames = classNames(styles.bar, displayClassNames, styles.skeleton);
 
-    const width = useMemo(
-        () => props.width || Math.max(Math.min(Math.floor(Math.random() * 100), 100), 50) + '%',
-        [props.width]
+    const barWidth = useMemo(
+        () => width || Math.max(Math.min(Math.floor(Math.random() * 100), 100), 50) + '%',
+        [width]
     );
-    return <div className={barClassNames} style={{ width, height }} />;
+    return <div className={barClassNames} style={{ width: barWidth, height }} />;
 };
 
 export default SkeletonBar;
