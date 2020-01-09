74e1491f65493802c1d7c55a26c916633df12045
Martin Førre <martin.forre@bouvet.no>
2020-01-09 16:16:27 +0100

    Refactor in view checking to seperate hook

--- src/hooks/usePopoverRef/components/Container.tsx
+++ src/hooks/usePopoverRef/components/Container.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from '../styles.less';
+import Arrow from "./Arrow";
 import useElevationClassName from '../../useElevationClassName';
+import useInViewChecker from '../../useInViewChecker';
 
 export type PopoverPlacement = 'below' | 'above' | 'left' | 'right';
 export type PopoverJustification = 'start' | 'center' | 'end';
@@ -12,40 +14,6 @@ export type PopoverContainerProps = {
     centered?: boolean;
     title?: string;
     fillWithContent?: boolean;
-};
-
-const Arrow = () => {
-    return (
-        <svg
-            className={styles.arrow}
-            width="12"
-            height="8"
-            viewBox="0 0 12 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M9.79423 7.5L10.7152 7.5L10.2135 6.72765L7.15506 2.0188C6.56361 1.10821 5.23062 1.10821 4.63917 2.0188L1.58069 6.72765L1.07903 7.5L2 7.5L9.79423 7.5Z"
-                fill="white"
-                stroke="#E6E6E6"
-            />
-            <path
-                d="M5.29289 3.70711C5.68342 3.31658 6.31658 3.31658 6.70711 3.70711L11 8L1 8L5.29289 3.70711Z"
-                fill="white"
-            />
-        </svg>
-    );
-};
-
-const isCompletelyInView = (el: HTMLElement, margin = 1) => {
-    const rect = el.getBoundingClientRect();
-
-    return (
-        el.contains(document.elementFromPoint(rect.left + margin, rect.top + margin)) &&
-        el.contains(document.elementFromPoint(rect.left + margin, rect.bottom - margin)) &&
-        el.contains(document.elementFromPoint(rect.right - margin, rect.top + margin)) &&
-        el.contains(document.elementFromPoint(rect.right - margin, rect.bottom - margin))
-    );
 };
 
 const getNextAlternativePlacement = (
@@ -109,16 +77,7 @@ const PopoverContainer = React.forwardRef<
         }
     );
 
-    const getIsInView = () => {
-        const refObject = ref as React.MutableRefObject<HTMLDivElement>;
-        if (!refObject || !refObject.current) {
-            return true;
-        }
-
-        return isCompletelyInView(refObject.current, 4);
-    };
-
-    const [isInView, setIsInView] = React.useState(getIsInView());
+    const [isInView, setIsInView] = useInViewChecker(ref as React.MutableRefObject<HTMLDivElement>, 4);
     const [testedPlacements, setTestedPlacements] = React.useState<PopoverPlacement[]>([]);
     React.useEffect(() => {
         if (isInView || testedPlacements.length >= 4) {
@@ -129,32 +88,14 @@ const PopoverContainer = React.forwardRef<
             preferredPlacement || 'below',
             testedPlacements
         );
+
         setTestedPlacements([...testedPlacements, nextPlacement]);
         setPlacement(nextPlacement);
         setIsInView(true);
     }, [isInView]);
 
-    const animationFrame = React.useRef(0);
-    const checkIsInView = () => {
-        window.cancelAnimationFrame(animationFrame.current);
-
-        setIsInView(getIsInView());
-
-        animationFrame.current = window.requestAnimationFrame(checkIsInView);
-    };
-
-    React.useEffect(() => {
-        checkIsInView();
-        return () => {
-            window.cancelAnimationFrame(animationFrame.current);
-        };
-    }, []);
-
     return (
-        <div
-            className={containerClassNames}
-            ref={ref as React.Ref<HTMLDivElement>}
-        >
+        <div className={containerClassNames} ref={ref as React.Ref<HTMLDivElement>}>
             <Arrow />
             {title && <h5>{title}</h5>}
             <div className={styles.content}>{children}</div>
