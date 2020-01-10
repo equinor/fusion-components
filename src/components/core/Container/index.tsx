6df841416f91bf144c37fd8a76d82f36ce6731ab
Martin Førre <martin.forre@bouvet.no>
2020-01-10 08:45:34 +0100

    Add support for rendering the fusion root and container without fusion header

--- src/components/core/Container/index.tsx
+++ src/components/core/Container/index.tsx
@@ -3,12 +3,20 @@ import classNames from 'classnames';
 import { useComponentDisplayClassNames } from '@equinor/fusion';
 import styles from './styles.less';
 
-type FusionContainerProps = {};
+type FusionContainerProps = {
+    noHeader?: boolean;
+};
 
-const FusionContainer = React.forwardRef<HTMLDivElement, React.Props<FusionContainerProps>>(
-    ({ children }, ref) => {
-        const containerClassNames = classNames(styles.container, useComponentDisplayClassNames(styles));
-        
+const FusionContainer = React.forwardRef<HTMLDivElement, React.PropsWithChildren<FusionContainerProps>>(
+    ({ noHeader = false, children }, ref) => {
+        const containerClassNames = classNames(
+            styles.container,
+            useComponentDisplayClassNames(styles),
+            {
+                [styles.noHeader]: noHeader,
+            }
+        );
+
         return (
             <div className={containerClassNames} ref={ref}>
                 {children}
