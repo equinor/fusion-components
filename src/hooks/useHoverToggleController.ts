e433df45f488e8f9990672fbd0d06578d3156447
Stian Sandve <stian@sandve.org>
2020-02-07 15:50:34 +0100

    Fixes problem where multiple tooltips was rendered on top of each other

--- src/hooks/useHoverToggleController.ts
+++ src/hooks/useHoverToggleController.ts
@@ -1,43 +1,38 @@
-import { useRef, useState, MutableRefObject, useCallback } from 'react';
+import { useRef, useState, MutableRefObject, useCallback, useEffect } from 'react';
 import { useEventListener } from '@equinor/fusion-components';
+
+let showTimeout: NodeJS.Timeout;
 export default <T extends HTMLElement>(
     delay: number = 300
 ): [boolean, MutableRefObject<T | null>] => {
     const [isHovering, setIsHovering] = useState<boolean>(false);
     const ref = useRef<T | null>(null);
-    const timer = useRef<NodeJS.Timeout>();
 
     const show = useCallback(() => {
-        if (timer.current) clearTimeout(timer.current);
-        timer.current = setTimeout(() => setIsHovering(true), delay);
-    }, [delay, timer.current]);
+        clearTimeout(showTimeout);
+        showTimeout = setTimeout(() => setIsHovering(true), delay);
+    }, [delay]);
 
     const hide = useCallback(() => {
-        if (timer.current) clearTimeout(timer.current);
-        if (isHovering) {
-            setIsHovering(false);
-        }
-    }, [isHovering, timer.current]);
+        clearTimeout(showTimeout);
+        setIsHovering(false);
+    }, []);
 
     const checkShouldShow = useCallback(
         (e: Event) => {
-            if (!ref.current) {
-                hide();
+            if (!ref.current || !e.target) {
+                if (isHovering) {
+                    hide();
+                }
+
                 return;
             }
-            const mouseEvent = e as MouseEvent;
-            const hoveredElement = document.elementFromPoint(mouseEvent.pageX, mouseEvent.pageY);
-            const isWithinRef =
-                ref.current == hoveredElement || ref.current.contains(hoveredElement);
-            if (isWithinRef && !isHovering) {
-                show();
-            } else if (!isWithinRef) {
-                hide();
-            }
         },
-        [show, hide, ref.current, isHovering]
+        [hide, ref.current, isHovering]
     );
 
+    useEventListener(ref.current, 'mouseenter', show, [ref.current]);
+    useEventListener(ref.current, 'mouseleave', hide, [ref.current]);
     useEventListener(window, 'mousemove', checkShouldShow, [checkShouldShow]);
 
     return [isHovering, ref];
