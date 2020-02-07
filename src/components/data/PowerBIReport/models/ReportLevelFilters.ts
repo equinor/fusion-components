b7b783025f7b6006fbe006662a1821092c6562b1
Ole-Richard Blindheim <oblin@equinor.com>
2020-02-07 12:54:43 +0100

    Fixed export off component and models (#255)

--- src/components/data/PowerBIReport/models/ReportLevelFilters.ts
+++ src/components/data/PowerBIReport/models/ReportLevelFilters.ts
@@ -1,11 +1,9 @@
 import * as models from 'powerbi-models';
 
-type ReportLevelFilters = models.ReportLevelFilters | null;
+export type ReportLevelFilters = models.ReportLevelFilters | null;
 
 export type IBasicFilter = models.IBasicFilter;
 export type IBasicFilterWithKeys = models.IBasicFilterWithKeys;
 export type IAdvancedFilter = models.IAdvancedFilter;
 export type IRelativeDateFuilter = models.IRelativeDateFilter;
 export type ITupleFilter = models.ITupleFilter;
-
-export default ReportLevelFilters;
