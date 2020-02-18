0868987c1e2426f2c76d3f744795af3c8f18aa5b
Eskil Lindløv Sand <eslsa@equinor.com>
2020-02-18 12:59:41 +0100

    Adaptive card viewer (#325)
    * Adaptive card viewer
    
    * Added index export
    
    * Now uses effect to append child to adaptive card container
    
    * Removed child in cleanup function

---
+++ src/components/general/AdaptiveCardViewer/AdaptiveCardViewer.stories.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import withFusionStory from '../../../../.storybook/withFusionStory';
+import AdaptiveCardViewer from '.';
+import { action } from '@storybook/addon-actions';
+
+var payload = {
+    type: 'AdaptiveCard',
+    version: '1.0',
+    body: [
+        {
+            type: 'Image',
+            url: 'http://adaptivecards.io/content/adaptive-card-50.png',
+        },
+        {
+            type: 'TextBlock',
+            text: `# Hello Adaptive card \n ## This is markdown \n ### This is also markdown \n\n * Some \n * List \n * Items`,
+        },
+    ],
+    actions: [
+        {
+            type: 'Action.Submit',
+            title: 'Submit',
+            url: 'http://adaptivecards.io',
+        },
+        {
+            type: 'Action.OpenUrl',
+            title: 'GitHub',
+            url: 'http://github.com/Microsoft/AdaptiveCards',
+        },
+    ],
+};
+const AdaptiveCardViewerStory = () => {
+    return (
+        <div style={{ margin: '8px' }} >
+            <AdaptiveCardViewer payload={payload} onActionSubmit={action("Submit")}/>
+        </div>
+    );
+};
+
+storiesOf('General|Adaptive Card Viewer', module)
+    .addDecorator(withFusionStory('Adaptive Card Viewer'))
+    .add('Default', () => <AdaptiveCardViewerStory />);
