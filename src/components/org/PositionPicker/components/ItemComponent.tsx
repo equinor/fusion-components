3f12ba8c0383ceb988bbe012b31550ee819406d8
Martin Førre <mforr@equinor.com>
2020-04-06 21:08:38 +0200

    Handle null external id in position picker (#477)
    * Don't render externalId if it has no value/is null
    
    * Handle null/empty external id in dropdown aswell

--- src/components/org/PositionPicker/components/ItemComponent.tsx
+++ src/components/org/PositionPicker/components/ItemComponent.tsx
@@ -12,11 +12,17 @@ const ItemComponent: React.FC<ItemComponentProps> = ({ item }) => {
         i => now >= i.appliesFrom.getTime() && now <= i.appliesTo.getTime()
     );
 
+    const positionName = React.useMemo(() => {
+        if (!item.position.externalId) {
+            return item.position.name;
+        }
+
+        return `${item.position.externalId} - ${item.position.name}`;
+    }, [item.position.externalId, item.position.name]);
+
     return (
         <div className={styles.cardContainer}>
-            <div className={styles.positionName}>
-                {item.position.externalId} - {item.position.name}
-            </div>
+            <div className={styles.positionName}>{positionName}</div>
             <div className={styles.assignedPersonName}>
                 {activeInstance && activeInstance.assignedPerson
                     ? activeInstance.assignedPerson.name
