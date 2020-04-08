c5d661176fbad665d98557bece8d5eef4a7b0303
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-04-08 12:55:32 +0200

    Removed sanitizer

--- src/components/general/MarkdownViewer/index.tsx
+++ src/components/general/MarkdownViewer/index.tsx
@@ -7,20 +7,6 @@ type MarkdownViewerProps = {
 };
 
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ markdown }) => {
-    const sanitizer = () => {
-        // This function callback receives non-markdown tokens for
-        // examination. For instance, for the input "<html>some text</html>",
-        // this function will be called twice with "<html>" and "</html>" as tokens respectively.
-
-        // For now, we will handle all non-markdown tokens equally and simply strip them away.
-        return '';
-    };
-
-    marked.setOptions({
-        sanitize: true,
-        sanitizer,
-    });
-
     return (
         <div className={styles.container} dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
     );
