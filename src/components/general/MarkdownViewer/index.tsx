4dbf02846eaaeaff31b0e5d7e3c5768beb3dc620
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-04-08 14:55:59 +0200

    added dompurify

--- src/components/general/MarkdownViewer/index.tsx
+++ src/components/general/MarkdownViewer/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import marked from 'marked';
 import styles from './styles.less';
+import  dompurify from "dompurify";
 
 type MarkdownViewerProps = {
     markdown: string;
 };
 
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ markdown }) => {
+
     return (
         <div className={styles.container} dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
     );
