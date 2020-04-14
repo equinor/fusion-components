01185637a53d21d1a65ef4acb736cdc2f5173b90
Eskil Lindløv Sand <eslsa@equinor.com>
2020-04-14 11:18:23 +0200

    Markdown viewer - Update sanitizer (#485)
    * Removed sanitizer
    
    * added dompurify
    
    * Added domsinitizer
    
    * Added dompurify sanitizer

--- src/components/general/MarkdownViewer/index.tsx
+++ src/components/general/MarkdownViewer/index.tsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import marked from 'marked';
 import styles from './styles.less';
+import dompurify from 'dompurify';
 
 type MarkdownViewerProps = {
     markdown: string;
 };
 
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ markdown }) => {
-    const sanitizer = () => {
-        // This function callback receives non-markdown tokens for
-        // examination. For instance, for the input "<html>some text</html>",
-        // this function will be called twice with "<html>" and "</html>" as tokens respectively.
-
-        // For now, we will handle all non-markdown tokens equally and simply strip them away.
-        return '';
-    };
-
-    marked.setOptions({
-        sanitize: true,
-        sanitizer,
-    });
-
     return (
-        <div className={styles.container} dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
+        <div
+            className={styles.container}
+            dangerouslySetInnerHTML={{ __html: dompurify.sanitize(marked(markdown)) }}
+        />
     );
 };
 
