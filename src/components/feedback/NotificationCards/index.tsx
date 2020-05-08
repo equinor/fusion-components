ebd0f299d1da008d26e1f693053190fa4628a923
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-05-08 08:00:50 +0200

    Notificationcards component

---
+++ src/components/feedback/NotificationCards/index.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { NotificationCard, useComponentDisplayClassNames } from '@equinor/fusion';
+import * as styles from './styles.less';
+import classNames from 'classnames';
+import NotificationCardWrapper from './NotificationCardWrapper';
+
+type NotificationCardsProps = {
+    notifications: NotificationCard[];
+};
+type GradientType = 'top' | 'bottom' | 'topAndBottom' | null;
+
+const NotificationCards: React.FC<NotificationCardsProps> = ({ notifications }) => {
+    const [showCards, setShowCards] = React.useState<boolean>(false);
+    const notificationCardsRef = React.useRef<HTMLDivElement | null>(null);
+
+    const checkForGradient = React.useCallback((): GradientType => {
+        if (!notificationCardsRef.current) {
+            return null;
+        }
+
+        const pane = notificationCardsRef.current;
+
+        if (pane.scrollTop === 0 && pane.offsetHeight < pane.scrollHeight) {
+            return 'bottom';
+        }
+
+        if (pane.scrollTop != 0 && pane.scrollTop + pane.offsetHeight < pane.scrollHeight) {
+            return 'topAndBottom';
+        }
+        if (pane.scrollTop != 0 && pane.scrollTop + pane.offsetHeight === pane.scrollHeight) {
+            return 'top';
+        }
+        return null;
+    }, [notificationCardsRef.current]);
+
+    const [gradient, setGradient] = React.useState<GradientType>(checkForGradient);
+
+    const containerClassNames = classNames(
+        styles.container,
+        useComponentDisplayClassNames(styles),
+        {
+            [styles.showGradientTop]: gradient === 'top' || gradient === 'topAndBottom',
+            [styles.showGradientBottom]: gradient === 'bottom' || gradient === 'topAndBottom',
+        }
+    );
+
+    React.useEffect(() => setGradient(checkForGradient), [showCards]);
+
+    React.useEffect(() => {
+        if (notifications.length > 0 && !showCards) {
+            setShowCards(true);
+        }
+        if (!(notificationCardsRef && notificationCardsRef.current)) {
+            return;
+        }
+        notificationCardsRef.current.scrollTop = 0;
+    }, [notifications]);
+
+    return (
+        <div
+            className={containerClassNames}
+            ref={notificationCardsRef}
+            onScroll={() => setGradient(checkForGradient)}
+        >
+            <div className={styles.gradientTop} />
+
+            {notifications.map((notification) => (
+                <NotificationCardWrapper notification={notification} />
+            ))}
+
+            <div className={styles.gradientBottom} />
+        </div>
+    );
+};
+export default NotificationCards;
