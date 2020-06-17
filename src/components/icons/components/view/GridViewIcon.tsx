ced88872551db4346702dc159ff324e39543080e
odin thomas rochmann <odin.rochmann@gmail.com>
2020-06-17 10:22:52 +0200

    fix: kebab to camlcase on svg attributes

--- src/components/icons/components/view/GridViewIcon.tsx
+++ src/components/icons/components/view/GridViewIcon.tsx
@@ -4,8 +4,8 @@ import { IconProps, useIcon } from '@equinor/fusion-components';
 export const GridViewIcon = (props: IconProps) => {
     const iconFactory = useIcon(
         <path 
-            fill-rule="evenodd" 
-            clip-rule="evenodd" 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
             fill="currentColor"
             d="M3.5 5.5V18.5H20.5V5.5H3.5ZM13.5 7.5V11H10.5V7.5H13.5ZM8.5 7.5H5.5V11H8.5V7.5ZM5.5 16.5V13H8.5V16.5H5.5ZM10.5 13V16.5H13.5V13H10.5ZM18.5 16.5H15.5V13H18.5V16.5ZM15.5 7.5V11H18.5V7.5H15.5Z" 
         />
--- src/components/icons/components/view/ListViewIcon.tsx
+++ src/components/icons/components/view/ListViewIcon.tsx
@@ -3,7 +3,12 @@ import { IconProps, useIcon } from '@equinor/fusion-components';
 
 export const ListViewIcon = (props: IconProps) => {
     const iconFactory = useIcon(
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7H5V9H3V7ZM5 11H3V13H5V11ZM21 11H7V13H21V11ZM3 15H5V17H3V15ZM21 15H7V17H21V15ZM21 7H7V9H21V7Z" fill="currentColor"/>
+        <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            fill="currentColor"
+            d="M3 7H5V9H3V7ZM5 11H3V13H5V11ZM21 11H7V13H21V11ZM3 15H5V17H3V15ZM21 15H7V17H21V15ZM21 7H7V9H21V7Z" 
+        />
     );
 
     return iconFactory(props);
