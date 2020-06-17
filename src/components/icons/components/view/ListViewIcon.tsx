0112795f8ddda5ff6a0ab293abc494972bd13a9a
odinr <odin.rochmann@gmail.com>
2020-06-17 10:48:19 +0200

    Feature/list view icons (#610)
    * feat: add list icons
    
    + icon for list view
    + icon for grid view
    + icon for column view
    + story for list icons
    
    * fix: kebab to camlcase on svg attributes

---
+++ src/components/icons/components/view/ListViewIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { IconProps, useIcon } from '@equinor/fusion-components';
+
+export const ListViewIcon = (props: IconProps) => {
+    const iconFactory = useIcon(
+        <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            fill="currentColor"
+            d="M3 7H5V9H3V7ZM5 11H3V13H5V11ZM21 11H7V13H21V11ZM3 15H5V17H3V15ZM21 15H7V17H21V15ZM21 7H7V9H21V7Z" 
+        />
+    );
+
+    return iconFactory(props);
+};
+
+export default ListViewIcon;
