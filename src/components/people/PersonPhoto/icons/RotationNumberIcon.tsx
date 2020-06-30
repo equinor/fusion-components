9ef1e0d81a6f75f9f85d41507ec13456e2d30e86
Martin Førre <mforr@equinor.com>
2020-06-30 08:10:47 +0200

    Bugfix/invalid jsx props (#630)
    * Fix employee icon
    
    * Fix rotation number icon

--- src/components/people/PersonPhoto/icons/RotationNumberIcon.tsx
+++ src/components/people/PersonPhoto/icons/RotationNumberIcon.tsx
@@ -9,7 +9,7 @@ const RotationNumberIcon = (props: RotationNumberIconProps) => {
     const iconFactory = useIcon(
         <>
             <circle cx="12" cy="12" r="12" fill="#007079" />
-            <text x="50%" y="50%" text-anchor="middle" stroke="white" stroke-width="1px" dy=".3em">
+            <text x="50%" y="50%" text-anchor="middle" stroke="white" strokeWidth="1px" dy=".3em">
                 {props.numberOfPersons}
             </text>
         </>
