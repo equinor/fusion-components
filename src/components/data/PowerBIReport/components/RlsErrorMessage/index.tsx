40075f85f634bb1323706c572d9a7ad8091e170c
Ole-Richard Blindheim <post@olerichard.org>
2020-08-13 11:05:26 +0200

    PowerBI Report component update.   - Update fusion api package   - updated report component to reflect changes in fusion api package.   - api update mostly affects model changes.   - Uses of any has been removed.   - new RLS Error handling   - RLS will not be accounted for and will show its own error message when user dont have access.   - Other errors still uses old standard error message.   - loading spinner implementations has been reworked.   - the clause for showing spinner didnt acutally make much sense and has been corrected.   - also added a chaning loading message. Fetching toking can sometimes take abit of time. This call also retries on fail. This information atleast gives the user a sense of whats going on. Also its usefull information for us if its a problem that gets reported in.

---
+++ src/components/data/PowerBIReport/components/RlsErrorMessage/index.tsx
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import * as styles from './styles.less';
+import { Report } from '@equinor/fusion/lib/http/apiClients/models/report/';
+import {
+    MarkdownViewer,
+    Spinner,
+    Button,
+    PersonCard,
+    Accordion,
+    AccordionItem,
+    useElevationClassName,
+} from '@equinor/fusion-components';
+import { useCurrentUser, useApiClients } from '@equinor/fusion';
+import classNames from 'classnames';
+
+type RlsErrorMessageProps = {
+    report: Report;
+};
+
+const RlsErrorMessage: React.FC<RlsErrorMessageProps> = ({ report }) => {
+    const [isFetching, setIsFetching] = React.useState<boolean>(true);
+    const [requirements, setRequirements] = React.useState<string | null>(null);
+    const [description, setDescription] = React.useState<string | null>(null);
+    const [noAccessMessage, setNoAccessMessage] = React.useState<string | null>(null);
+    const [isAccessControlDescriptionsOpen, setAccessControlDescriptionOpen] = React.useState(
+        false
+    );
+
+    const reportApiClient = useApiClients().report;
+    const user = useCurrentUser();
+    const timeStamp = React.useMemo(() => new Date().toString(), []);
+
+    React.useEffect(() => {
+        getRlsRequirements();
+    }, []);
+
+    const getRlsRequirements = async () => {
+        setIsFetching(true);
+        const fetchedRequirements = await reportApiClient.getRlsRequirements(report.id);
+        const fetchedNoAccessMessage = await reportApiClient.getAccessDescription(report.id);
+        const fetchedDescriptions = await reportApiClient.getDescription(report.id);
+
+        setNoAccessMessage(fetchedNoAccessMessage?.data || null);
+        setDescription(fetchedDescriptions?.data || null);
+        setRequirements(fetchedRequirements?.data || null);
+        setIsFetching(false);
+    };
+
+    if (isFetching)
+        return <Spinner title={'Retrieving report access specifications '} floating centered />;
+
+    return (
+        <div className={styles.rlsErroMessage}>
+            <div className={styles.container}>
+                <h2 className={styles.header}>Restricted Access</h2>
+                <div
+                    className={classNames(
+                        useElevationClassName(3),
+                        styles.restrictedAccessContainer
+                    )}
+                >
+                    <h2>It looks like you do not have access to this report</h2>
+                    {noAccessMessage && <MarkdownViewer markdown={noAccessMessage} />}
+                    <div className={styles.reportInfoContainer}>
+                        {description && (
+                            <div className={styles.reportDescription}>
+                                <h5>Report description</h5>
+                                <MarkdownViewer markdown={description} />
+                            </div>
+                        )}
+                        {report.ownedBy?.azureUniqueId && (
+                            <div className={styles.reportOwner}>
+                                <h5>Report owner</h5>
+                                <PersonCard personId={report.ownedBy.azureUniqueId}></PersonCard>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <div className={styles.userIdentityContainer}>
+                    <h4>This report is configured to use Power BI RLS and your identity is:</h4>
+                    <div className={styles.info}>
+                        <div className={styles.labels}>
+                            <div>Username</div>
+                            <div>GUID</div>
+                            <div>Roles</div>
+                            <div>Timestamp</div>
+                            <div>Context</div>
+                        </div>
+                        <div className={styles.text}>
+                            <div> {user?.upn} </div>
+                            <div> {user?.id} </div>
+                            <div>{user?.roles.toString()} </div>
+                            <div> {timeStamp} </div>
+                            <div> {window.location.href} </div>
+                        </div>
+                    </div>
+                </div>
+                {requirements && (
+                    <div className={styles.accessControlContainer}>
+                        <Accordion>
+                            <AccordionItem
+                                label={'Access control description'}
+                                isOpen={isAccessControlDescriptionsOpen}
+                                onChange={() => setAccessControlDescriptionOpen((s) => !s)}
+                            >
+                                <MarkdownViewer markdown={requirements} />
+                            </AccordionItem>
+                        </Accordion>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default RlsErrorMessage;
