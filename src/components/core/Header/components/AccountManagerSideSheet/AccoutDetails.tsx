94faf0796c7252ef13bef06d39b4fb9205ce14f2
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-08-19 13:15:23 +0200

    Removed logout button

--- src/components/core/Header/components/AccountManagerSideSheet/AccoutDetails.tsx
+++ src/components/core/Header/components/AccountManagerSideSheet/AccoutDetails.tsx
@@ -1,20 +1,7 @@
 import * as React from 'react';
 import * as styles from './styles.less';
-import {
-    PersonDetails,
-    UserMenuSectionItem,
-    UserMenuSection,
-    useFusionContext,
-} from '@equinor/fusion';
-import {
-    MenuItemType,
-    PersonPhoto,
-    PersonDetail,
-    Button,
-    ExitToAppIcon,
-    Menu,
-    styling,
-} from '@equinor/fusion-components';
+import { PersonDetails, UserMenuSectionItem, UserMenuSection } from '@equinor/fusion';
+import { MenuItemType, PersonPhoto, PersonDetail, Menu } from '@equinor/fusion-components';
 
 type AccountDetailsProps = {
     personDetails: PersonDetails;
@@ -39,9 +26,6 @@ const AccountDetails: React.FC<AccountDetailsProps> = ({
     personDetails,
     sections,
 }) => {
-    const { auth } = useFusionContext();
-    const signOutUser = React.useCallback(async () => await auth.container.logoutAsync(), [auth]);
-
     const onMenuItemClick = React.useCallback(
         (item: MenuItemType) => {
             const clickedItem = item as UserMenuSectionItem;
@@ -59,12 +43,7 @@ const AccountDetails: React.FC<AccountDetailsProps> = ({
             <div className={styles.personDetails}>
                 <PersonDetail person={personDetails} noPhoto />
             </div>
-            <div className={styles.signOutContainer}>
-                <Button frameless onClick={signOutUser}>
-                    <span className={styles.signOutLabel}>Sign out</span>
-                    <ExitToAppIcon color={styling.colors.blackAlt2} />
-                </Button>
-            </div>
+
             <div className={styles.menuContainer}>
                 <Menu
                     sections={sections}
