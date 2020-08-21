8752e26f1dd9bff044d652fb90c8612ad31504a1
Ole-Richard Blindheim <post@olerichard.org>
2020-08-21 15:03:10 +0200

    a potential way to extract the markdown out of the component. Send in a function that will get the markdown sendt in as a parameter. This way the onChange effect could just be a setState funtion.

--- src/components/general/MarkdownEditor/MarkdownEditor.stories.tsx
+++ src/components/general/MarkdownEditor/MarkdownEditor.stories.tsx
@@ -7,9 +7,15 @@ import '.';
 const initialMarkdown = `# This is markdown \n ## This is also markdown \n ### This is also markdown \n\n * Some \n * List \n * Items`;
 
 const MarkdownEditorStory = () => {
+    const [markdown, setMarkDown] = React.useState(initialMarkdown);
+
+    React.useEffect(() => {
+        console.log('Markdown', markdown);
+    }, [markdown]);
+
     return (
         <>
-            <MarkdownEditor value={initialMarkdown} onChange={(e) => ''} />
+            <MarkdownEditor value={markdown} onChange={setMarkDown} />
         </>
     );
 };
