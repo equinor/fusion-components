1f0936d7d86110c411413794dcd489590032dc26
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-09-11 10:14:34 +0200

    chore: Update to storybook 6

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const styles = require('../build/style');
+const fonts = require('../build/fonts');
+const eslint = require('../build/eslint');
+const resolve = require('../build/resolve');
+const typescript = require('../build/typescript');
+
+module.exports = {
+    stories: ['../src/**/*.stories.@(jsx|tsx)'],
+    addons: [
+        '@storybook/addon-options/register',
+        '@storybook/addon-actions/register',
+        '@storybook/addon-jest/register',
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-viewport/register',
+    ],
+    webpackFinal: async (config) =>
+        merge(config, styles, resolve, fonts,eslint,typescript('../'),{
+            resolve: {
+                alias: {
+                    '@equinor/fusion-components': path.resolve(__dirname, '..', 'src', 'index.ts'),
+                },
+            },
+        }),
+};
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     presets: ['@babel/preset-typescript', '@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: ['@babel/plugin-proposal-decorators', '@babel/plugin-proposal-class-properties'],
     env: {
         test: {
             plugins: ['require-context-hook'],
