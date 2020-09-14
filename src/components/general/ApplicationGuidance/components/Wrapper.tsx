c1db2db0a3856e99a18df26908b59e7639b837c2
odin thomas rochmann <odin.rochmann@gmail.com>
2020-09-14 14:04:12 +0200

    feat: add events to wrapper

--- src/components/general/ApplicationGuidance/components/Wrapper.tsx
+++ src/components/general/ApplicationGuidance/components/Wrapper.tsx
@@ -3,11 +3,20 @@ import {
     ApplicationGuideElement,
     ApplicationGuideElementProps,
     ApplicationGuideElementTag,
+    ApplicationGuideEvent,
+    ApplicationGuideEventType,
 } from '../../../../customElements/components/application-guide';
 
 export type ApplicationGuidanceWrapperProps = React.PropsWithChildren<
     ApplicationGuideElementProps &
-    React.DetailedHTMLProps<React.HTMLAttributes<ApplicationGuideElement>, ApplicationGuideElement>
+        React.DetailedHTMLProps<
+            React.HTMLAttributes<ApplicationGuideElement>,
+            ApplicationGuideElement
+        > & {
+            onOpen?: (e: ApplicationGuideEvent<ApplicationGuideEventType.activated>) => void;
+            onClose?: (e: ApplicationGuideEvent<ApplicationGuideEventType.deactivated>) => void;
+            onShow?: (e: ApplicationGuideEvent<ApplicationGuideEventType.show>) => void;
+        }
 >;
 
 declare global {
@@ -28,9 +37,23 @@ declare global {
 }
 
 export const ApplicationGuidanceWrapper: React.FC<ApplicationGuidanceWrapperProps> = (
-    props: ApplicationGuidanceWrapperProps
+    args: ApplicationGuidanceWrapperProps
 ) => {
-    return <fusion-application-guide {...props}></fusion-application-guide>;
+    const { onOpen, onClose, onShow, ...props } = args;
+    const ref = React.useRef<ApplicationGuideElement>();
+    React.useEffect(() => {
+        if (!ref.current) return;
+        const el = ref.current;
+        onOpen && el.addEventListener(ApplicationGuideEventType.activated, onOpen);
+        onClose && el.addEventListener(ApplicationGuideEventType.deactivated, onClose);
+        onShow && el.addEventListener(ApplicationGuideEventType.show, onShow);
+        return () => {
+            onOpen && el.removeEventListener(ApplicationGuideEventType.activated, onOpen);
+            onClose && el.removeEventListener(ApplicationGuideEventType.deactivated, onClose);
+            onShow && el.removeEventListener(ApplicationGuideEventType.show, onShow);
+        };
+    }, [ref]);
+    return <fusion-application-guide ref={ref} {...props}></fusion-application-guide>;
 };
 
 export default ApplicationGuidanceWrapper;
