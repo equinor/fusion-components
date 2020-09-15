a014d31f721bcf13a28a8ecb06ad063b24352e7a
Ole-Richard Blindheim <post@olerichard.org>
2020-09-15 12:50:58 +0200

    Moved popover into their own components

---
+++ src/components/data/HangingGarden/__stories__/WorkOrderGarden/components/HoldStatusPopover/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as styles from '../../garden/styles.less';
+
+const HoldStatusPopover = () => {
+    return (
+        <div className={styles.woPopover}>
+            <h4>Hold status</h4>
+            <ul>
+                <li>
+                    <span className={styles.flagSpacer} /> No hold
+                </li>
+                <li>
+                    <span className={styles.redFlag} /> Hold by ENG, EWP, JLL, MAT, MC, OFF, PRE, WO
+                </li>
+                <li>
+                    <span className={styles.yellowFlag} /> Hold by MATB
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default HoldStatusPopover;
--- src/components/data/HangingGarden/__stories__/WorkOrderGarden/garden/index.tsx
+++ src/components/data/HangingGarden/__stories__/WorkOrderGarden/garden/index.tsx
@@ -20,6 +20,9 @@ import {
 import { getYearAndWeekFromDate, getYearAndWeek, getColumns } from '../columns/columns';
 import { FilterTerm, ItemRenderContext, HangingGarden } from '../../../../../..';
 import { HeaderRenderContext } from '../../../HangingGardenModels';
+import MaterialStatusPopover from '../components/MaterialStatusPopover';
+import ProjectPopover from '../components/ProjectPopover';
+import HoldStatusPopover from '../components/HoldStatusPopover';
 
 type GardenProps = {
     filteredData: WorkOrderType[];
@@ -154,118 +157,17 @@ const Garden: React.FC<GardenProps> = ({
         context.addDot(getMatStatusColor(item), { x: context.width - 12, y: 8 });
         context.addDot(getMccrStatusColor(item), { x: context.width - 12, y: context.height - 8 });
         renderItemProgress(item, context);
-        context.addPopover(new PIXI.Rectangle(0, 0, 32, context.height), () => {
-            return (
-                <div className={styles.woPopover}>
-                    <h4>Hold status</h4>
-                    <ul>
-                        <li>
-                            <span className={styles.flagSpacer} /> No hold
-                        </li>
-                        <li>
-                            <span className={styles.redFlag} /> Hold by ENG, EWP, JLL, MAT, MC, OFF,
-                            PRE, WO
-                        </li>
-                        <li>
-                            <span className={styles.yellowFlag} /> Hold by MATB
-                        </li>
-                    </ul>
-                </div>
-            );
-        });
+        context.addPopover(new PIXI.Rectangle(0, 0, 32, context.height), () => (
+            <HoldStatusPopover />
+        ));
 
         context.addPopover(
             new PIXI.Rectangle(32, 0, context.width - 32 - 24, context.height),
-            () => {
-                return (
-                    <div className={styles.woPopover}>
-                        <section>
-                            <div>
-                                <ul>
-                                    <li>
-                                        <b>Project(ProCoSys):</b>
-                                    </li>
-                                    <li>
-                                        {`${item.projectIdentifier}, ${item.projectDescription}`}
-                                    </li>
-                                </ul>
-                            </div>
-                        </section>
-                    </div>
-                );
-            }
+            () => <ProjectPopover item={item} />
         );
 
         context.addPopover(new PIXI.Rectangle(context.width - 24, 0, 24, context.height), () => (
-            <div className={styles.woPopover}>
-                <section>
-                    <div>
-                        <h4>Material status</h4>
-                        <ul>
-                            <li>
-                                <span className={styles.dot} style={{ background: '#00FF00' }} /> OK
-                            </li>
-                            <li>
-                                <span
-                                    className={styles.dot}
-                                    style={{
-                                        background: '#' + MAT_STATUS_COLORS.AVAILABLE.toString(16),
-                                    }}
-                                />{' '}
-                                Available
-                            </li>
-                            <li>
-                                <span
-                                    className={styles.dot}
-                                    style={{
-                                        background:
-                                            '#' + MAT_STATUS_COLORS.NOT_AVAILABLE.toString(16),
-                                    }}
-                                />{' '}
-                                Not Available
-                            </li>
-                            <li>
-                                <span className={styles.dot} style={{ background: '#AFAFAF' }} /> OS
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h4>MCCR status</h4>
-                        <ul>
-                            <li>
-                                <span className={styles.dot} style={{ background: '#00FF00' }} /> OK
-                            </li>
-                            <li>
-                                <span
-                                    className={styles.dot}
-                                    style={{ background: '#' + mccrColorMap.PA.toString(16) }}
-                                />{' '}
-                                PA
-                            </li>
-                            <li>
-                                <span
-                                    className={styles.dot}
-                                    style={{ background: '#' + mccrColorMap.PB.toString(16) }}
-                                />{' '}
-                                PB
-                            </li>
-                            <li>
-                                <span
-                                    className={styles.dot}
-                                    style={{ background: '#' + mccrColorMap.OS.toString(16) }}
-                                />{' '}
-                                OS
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-                <h4>Size</h4>
-                <div className={styles.list}>
-                    <span> Small</span>
-                    <span>| Medium</span>
-                    <span>|| Large</span>
-                </div>
-            </div>
+            <MaterialStatusPopover />
         ));
     };
 
