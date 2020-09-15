c20776bac3a461263a7e5a1909ddf8d87184f56e
Eskil Lindløv Sand <eslsa@equinor.com>
2020-09-15 10:54:01 +0200

    Feature - Rich text editor (#757)
    * New line
    
    * Moved text editor to components folder
    
    * MarkdownEditor component working decently.
    MarkdownEditor story actually showing something
    
    * Changed text color
    
    * a potential way to extract the markdown out of the component.
    Send in a function that will get the markdown sendt in as a parameter.
    This way the onChange effect could just be a setState funtion.
    
    * minor fixes
    
    * Updated story with new prop name
    
    * custom menu items
    
    * Re-wrote general use of element, moved svgs change css etc
    
    * Added active state to em and bold
    
    * Added children to markdown ediotor react component
    
    * Dynamic height
    
    * getMenuItemByType name fix
    
    * Code review fixes
    
    Co-authored-by: Ole-Richard Blindheim <post@olerichard.org>

---
+++ src/customElements/components/markdown-editor/element.css.ts
@@ -0,0 +1,94 @@
+import { css } from '../base';
+
+export const style = css`
+    :host {
+        --editor-color-background: var(--color-black-alt5);
+        --editor-color-text: var(--color-primary);
+        --editor-color-active: var(--color-primary-hover-alt1)
+        --grid-unit: 8px;
+        --border-radius: 4px;
+    }
+
+    .container{
+        height: 100%;
+    }
+
+    #editor {
+        padding: calc(var(--grid-unit) * 1px);
+        height: calc(100% - 45px);
+    }
+
+    #editor p {
+        font-size: 16px;
+        margin: var(--grid-unit) 0;
+    }
+
+    #editor p:last-child {
+        margin-bottom: 0;
+    }
+
+    #editor p:first-child {
+        margin-top: 0;
+    }
+
+    .ProseMirror {
+        background: var(--editor-color-background);
+        border-radius: var(--border-radius) var(--border-radius) 0 0;
+        color: var(--editor-color-text);
+        border-bottom: 1px solid var(--color-black-alt3);
+        padding: var(--grid-unit);
+        white-space: pre-wrap;
+        height: 100%;
+        overflow: auto;
+    }
+
+    .ProseMirror > *:first-child {
+        margin-top: 0;
+    }
+
+    .ProseMirror-focused {
+        outline: none;
+        border-color: var(--color-contrast);
+    }
+
+    #menu {
+        display: flex;
+        padding: calc(var(--grid-unit) * 0.5);
+        background: none;
+    }
+
+    #menu .button {
+        background: none;
+        border: none;
+        border-radius: var(--border-radius);
+        color: var(--color-contrast);
+        cursor: pointer;
+        font-family: Equinor, sans-serif;
+    }
+
+    #menu .button:hover {
+        color: var(--color-primary);
+    }
+
+    #menu .button.disabled {
+        background: var(--editor-color-background);
+        cursor: not-allowed;
+    }
+
+    #menu .button.active {
+        color: var(--editor-color-text);
+    }
+
+    #menu .button.heading {
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    #menu .button.paragraph {
+        font-weight: 600;
+    }
+`;
+
+export default [
+    style
+];
---
+++ src/customElements/components/markdown-editor/element.ts
@@ -0,0 +1,96 @@
+import { fusionElement, LitElement, property, html } from '../base';
+
+import { baseKeymap } from 'prosemirror-commands';
+import { keymap } from 'prosemirror-keymap';
+import { schema, defaultMarkdownSerializer, defaultMarkdownParser } from 'prosemirror-markdown';
+import { history } from 'prosemirror-history';
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { buildKeymap } from './keymap';
+import menuPlugin from './menu';
+
+import styles from './element.css';
+import { MdMenuItemType } from './menuItems';
+
+export interface MarkdownEditorElementProps {
+    menuItems?: Array<MdMenuItemType>;
+}
+
+const defaultMenuItem: Array<MdMenuItemType> = ['strong', 'em', 'bullet_list', 'ordered_list'];
+
+@fusionElement('fusion-markdown-editor')
+export class MarkdownEditorElement extends LitElement implements MarkdownEditorElementProps {
+    static styles = styles;
+
+    @property({ reflect: false, type: Array, converter: (a) => a.split(',') })
+    menuItems: Array<MdMenuItemType>;
+
+    firstUpdated() {
+        const editor = this.shadowRoot.querySelector('#editor');
+        const menu = this.shadowRoot.querySelector('#menu');
+        const initialValue = this.innerHTML;
+        this.innerHTML = null;
+        this.initializeEditor(editor, menu, initialValue);
+    }
+
+    protected crateChangeEvent(markdown: string) {
+        return new CustomEvent('change', {
+            detail: markdown,
+            composed: true,
+            bubbles: true,
+        });
+    }
+
+    protected createEditorState(
+        mdMenuItems: MdMenuItemType[],
+        menu: Element,
+        initialValue: string
+    ) {
+        return EditorState.create({
+            schema,
+            doc: defaultMarkdownParser.parse(initialValue),
+            plugins: [
+                history(),
+                keymap(buildKeymap(schema)),
+                keymap(baseKeymap),
+                menuPlugin(menu, mdMenuItems),
+            ],
+        });
+    }
+
+    protected createEditorView(editor: Element, state) {
+        const onChange = this.crateChangeEvent;
+        let editorView = new EditorView(editor, {
+            state,
+            dispatchTransaction(transaction) {
+                const changeEvent = onChange(defaultMarkdownSerializer.serialize(transaction.doc));
+                this.dispatchEvent(changeEvent);
+                const newState = editorView.state.apply(transaction);
+                editorView.updateState(newState);
+            },
+        });
+    }
+
+    private initializeEditor(editor: Element, menu: Element, initialValue: string) {
+        const mdMenuItems = this.menuItems || defaultMenuItem;
+        const state = this.createEditorState(mdMenuItems, menu, initialValue);
+        this.createEditorView(editor, state);
+    }
+
+    render() {
+        return html`
+            <div class="container">
+                <div id="menu"></div>
+                <div id="editor"></div>
+            </div>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'fusion-markdown-editor': MarkdownEditorElement;
+    }
+}
+
+export default MarkdownEditorElement;
