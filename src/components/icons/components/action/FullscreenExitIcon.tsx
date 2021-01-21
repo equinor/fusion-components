d8fd182e51ac257d958a9e172b7c12fb90e2a1e2
Ole-Richard Blindheim <post@olerichard.org>
2021-01-21 11:14:29 +0100

    Added new Icons FullscreenIcon and FullscreenExitIcon

---
+++ src/components/icons/components/action/FullscreenExitIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import useIcon, { IconProps } from '../../../../hooks/useIcon';
+
+const FullscreenExitIcon = (props: IconProps) => {
+    const iconFactory = useIcon(
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M5 8h3V5h2v5H5V8zm3 8H5v-2h5v5H8v-3zm6 3h2v-3h3v-2h-5v5zm2-14v3h3v2h-5V5h2z"
+            fill="currentColor"
+        />
+    );
+
+    return iconFactory(props);
+};
+
+export default FullscreenExitIcon;
