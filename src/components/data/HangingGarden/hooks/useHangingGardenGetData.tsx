6da4b0fa0f3b53e41d6ebac71b57affa709f53b5
odin thomas rochmann <odin.rochmann@gmail.com>
2021-02-10 13:50:04 +0100

    fix: broken merge

--- src/components/data/HangingGarden/hooks/useHangingGardenGetData.tsx
+++ src/components/data/HangingGarden/hooks/useHangingGardenGetData.tsx
@@ -1,124 +1,89 @@
-import { useCurrentContext, ApiClients } from '@equinor/fusion';
-import { useState, useEffect, useRef, useCallback } from 'react';
-import useHangingGardenGetData from './useHangingGardenGetData';
-import { formatDistance } from 'date-fns';
-
+import { useState, useCallback } from 'react';
+import { useApiClients, ApiClients } from '@equinor/fusion';
+import { HttpResponse } from '@equinor/fusion/lib/http/HttpClient';
+import { GardenDataError } from '../models/GardenDataError';
 /**
- * The useHangingGardenData hook fetched and stores your raw garden data. It also gives you cache invalidation capabilities and error handling. 
- * 
+ * The useHangingGardenGetData is used by useHangingGardenData for the acutal api call, but can be used on it own.
+ *
  * @param client Name of ApiClient to be used when fetching data.
  * @param endpoint Name of endpoint on given ApiClient to be used when fetching data.
- * @param applyToFetchedData Optional. A function that takes in and return the fetched data. Can be used if one wants "massage" the dat before returned
- * @param searchableValues Optional. A function that is used in a map to add a searchable field to each fetched item. 
- * @returns data: fetched data, array of T. error: null if OK, else a GardenError object. isFetching: fetching state. retry: a function to initiate a new fetch. Will first remove current data.
- * invalidate: a function to initiate a new fetch. Will NOT first remove current data. cacheIsInvalid: state of the cache, is invalid if cache is 30 min or older. cacheAge: age of cache in minutes
- * @example 
- *  
- * 
-export const setItemSearchableValues = (commpkg: HandoverPackage) => ({
-    ...commpkg,
-    searchableValues: [
-        commpkg.commpkgNo,
-        commpkg.description,
-        commpkg.area,
-        commpkg.responsible,
-    ].join(),
-});
- * 
- *  const applyToFetchedData = (data: HandoverPackage[]) => data.filter((item) => !item.isDemolition);
- * 
- * const {
-        data,
-        error,
-        isFetching,
-        retry,
-        invalidate,
-        cacheAge,
-        cacheIsInvalid,
-    } = useHangingGardenData(
-        'dataProxy',
-        'getHandoverAsync',
-        applyToFetchedData,
-        setItemSearchableValues
-    );
+ * @returns getData: takes in currentContext Id and InvalidateCache boolean. Does the api call and return the raw data. error: handles errors gives a GardenDataError or null. isFetching: a fetching flag boolean
+ * @example
+ * const { isFetching, error, getData } = useHangingGardenGetData('ApiClient', 'endpoint');
+ *
+ * const currentContext = useCurrentContext();
+ * const data = await getData(currentContext.id, true);
+ *
  */
-
-const useHangingGardenData = <T, C extends keyof ApiClients, E extends keyof ApiClients[C]>(
+const useHangingGardenGetData = <T, C extends keyof ApiClients, E extends keyof ApiClients[C]>(
     client: C,
-    endpoint: E,
-    applyToFetchedData?: ((data: T[]) => T[]) | null,
-    searchableValues?: ((data: T) => T) | null
+    endpoint: E
 ) => {
-    const currentContext = useCurrentContext();
-    const { isFetching, error, getData } = useHangingGardenGetData(client, endpoint);
-    const [data, setData] = useState<T[]>([]);
+    const api = useApiClients();
+    const [error, setError] = useState<GardenDataError | null>(null);
+    const [isFetching, setIsFetching] = useState(false);
 
-    const [cacheAgeDate, setCacheAgeDate] = useState<Date>(new Date());
-    const [cacheDuration, setCacheDuration] = useState<number>(30);
-    const [cacheAge, setCacheAge] = useState('');
-    const [cacheIsInvalid, setCacheIsInvalid] = useState(true);
-    const cacheCheckTimerRef = useRef<NodeJS.Timeout>();
+    const fetchData = useCallback(
+        async (currentContext: string, invalidateCache?: boolean) => {
+            setIsFetching(true);
+            try {
+                const response = (await (api[client][endpoint] as any)(
+                    currentContext,
+                    invalidateCache
+                )) as HttpResponse<T[]>;
 
-    const checkCacheValidity = useCallback(() => {
-        setCacheAge(formatDistance(cacheAgeDate, new Date()));
-        const isInvalid =
-            new Date(cacheAgeDate).setMinutes(cacheAgeDate.getMinutes() + cacheDuration) <
-            Date.now();
-        setCacheIsInvalid(isInvalid);
-        cacheCheckTimerRef.current = setTimeout(checkCacheValidity, 60000);
-    }, [cacheAgeDate, cacheDuration]);
+                if (response.status === 202) {
+                    setError({ errorType: 'noCache' });
+                    setIsFetching(false);
+                    return null;
+                }
 
-    useEffect(() => {
-        if (cacheCheckTimerRef.current) clearTimeout(cacheCheckTimerRef.current);
+                if (response.status === 200 && !response.data.length) {
+                    setError({ errorType: 'noData' });
+                    setIsFetching(false);
+                    return null;
+                }
 
-        checkCacheValidity();
+                const cacheAgeHeader = response.headers.get('x-pp-cache-age');
+                const cacheAge =
+                    cacheAgeHeader === null || cacheAgeHeader === 'live'
+                        ? new Date()
+                        : new Date(cacheAgeHeader);
+                const cacheDurationHeader = response.headers.get('x-pp-cache-duration-minutes');
+                const cacheDuration = parseInt(cacheDurationHeader || '30', 10);
 
-        return () => cacheCheckTimerRef.current && clearTimeout(cacheCheckTimerRef.current);
-    }, [checkCacheValidity]);
-
-    const formatData = useCallback(
-        (data: T[]) => {
-            const formattedData = applyToFetchedData ? applyToFetchedData(data) : data;
-            return searchableValues ? formattedData.map(searchableValues) : formattedData;
+                setIsFetching(false);
+                return {
+                    data: response.data,
+                    cacheAge,
+                    cacheDurationInMinutes: cacheDuration,
+                };
+            } catch (e) {
+                setError({
+                    errorType: e?.response?.error?.code || 'error',
+                    errorResponse: e?.response?.error || null,
+                });
+                setIsFetching(false);
+                return null;
+            }
         },
-        [searchableValues, applyToFetchedData]
+        [client, endpoint, api]
     );
 
-    const fetch = useCallback(
-        async (invalidateCache: boolean) => {
-            if (!currentContext) return;
-
-            const result = await getData(currentContext.id, invalidateCache);
-            setData(formatData((result?.data as T[]) || []));
-            setCacheAgeDate(result?.cacheAge || new Date());
-            setCacheDuration(result?.cacheDurationInMinutes || 30);
+    const getData = useCallback(
+        (currentContext: string, invalidateCache?: boolean) => {
+            setError(null);
+            setIsFetching(false);
+            return fetchData(currentContext, invalidateCache);
         },
-        [currentContext, isFetching, error, getData, formatData]
+        [fetchData]
     );
-
-    useEffect(() => {
-        setData([]);
-        fetch(false);
-    }, [currentContext?.id]);
-
-    const retry = useCallback(() => {
-        setData([]);
-        fetch(true);
-    }, [fetch]);
-
-    const invalidate = useCallback(() => {
-        fetch(true);
-    }, [fetch]);
 
     return {
-        data,
+        getData,
         error,
         isFetching,
-        retry,
-        invalidate,
-        cacheIsInvalid,
-        cacheAge,
     };
 };
 
-export default useHangingGardenData;
+export default useHangingGardenGetData;
