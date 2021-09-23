61eb2696aae8d34134077ce1a745bf2ed1ce77f7
tobiasny <tobnyg@hotmail.no>
2021-09-23 12:01:11 +0200

    fix defined twice issue

--- src/components/org/PositionTimeline/SelectableTimeline/PersonSlot.tsx
+++ src/components/org/PositionTimeline/SelectableTimeline/PersonSlot.tsx
@@ -2,6 +2,15 @@ import { PersonPhoto } from '@equinor/fusion-components';
 import { FC } from 'react';
 import { PersonSlotProps, TimelineSplit } from '../model';
 
+const _customElementsDefine = window.customElements.define;
+window.customElements.define = (name, cl, conf) => {
+    if (!customElements.get(name)) {
+        _customElementsDefine.call(window.customElements, name, cl, conf);
+    } else {
+        console.debug(`${name} has been defined twice`);
+    }
+};
+
 export const PersonSlot: FC<PersonSlotProps<TimelineSplit>> = ({ item }) => {
     return <PersonPhoto personId={item?.assignedPerson?.azureUniqueId} size="small" />;
 };
