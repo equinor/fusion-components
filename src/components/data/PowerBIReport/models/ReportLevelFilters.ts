a54bbda192c08dd878fc574b164df20a9dbb6c3b
AbdullZahir <Abdullah.zahir@hotmail.com>
2021-11-02 14:15:09 +0100

    fix: removed powerBIReports

--- src/components/data/PowerBIReport/models/ReportLevelFilters.ts
+++
@@ -1,9 +0,0 @@
-import * as models from 'powerbi-models';
-
-export type ReportLevelFilters = models.ReportLevelFilters | null;
-
-export type IBasicFilter = models.IBasicFilter;
-export type IBasicFilterWithKeys = models.IBasicFilterWithKeys;
-export type IAdvancedFilter = models.IAdvancedFilter;
-export type IRelativeDateFuilter = models.IRelativeDateFilter;
-export type ITupleFilter = models.ITupleFilter;
