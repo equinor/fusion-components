c701ec2e1e755a3a925051f490e9ad583c98a48a
AbdullZahir <Abdullah.zahir@hotmail.com>
2021-11-11 08:54:04 +0100

    fix: scrim style update

---
+++ src/components/general/Scrim/Scrim.style.ts
@@ -0,0 +1,37 @@
+import { createStyles, makeStyles } from '@equinor/fusion-react-styles';
+
+export const useStyles = makeStyles(
+    createStyles({
+        scrim: {
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            position: 'fixed',
+            opacity: 0,
+            transition: 'opacity 0.2s ease',
+            backgroundColor: 'rgba(0, 0, 0, 0.32)',
+        },
+        show: {
+            opacity: 1,
+            pointerEvents: 'all',
+        },
+    })
+);
+export default useStyles;
+
+// .scrim {
+//     top: 0;
+//     bottom: 0;
+//     left: 0;
+//     right: 0;
+//     position: fixed;
+//     opacity: 0;
+//     transition: opacity 0.2s ease;
+//     background-color: rgba(0, 0, 0, 0.32);
+
+//     &.show {
+//         opacity: 1;
+//         pointer-events: all;
+//     }
+// }
