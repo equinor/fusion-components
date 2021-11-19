d1ea8b2bbd99ee5465ce08cf5c8295347c60d5cd
olerichard <post@olerichard.org>
2021-11-19 09:38:05 +0100

    Saved files to get proper formatting

--- src/components/data/PowerBI/Report/store/reducers/context-access.ts
+++ src/components/data/PowerBI/Report/store/reducers/context-access.ts
@@ -2,34 +2,32 @@ import { createReducer } from 'typesafe-actions';
 
 import { State, Status, removeError, removeStatus } from '../state';
 
-import {
-  ContextActions as Action,
-  checkContextAccess,
-  setContextAccess
-} from '../actions/context';
+import { ContextActions as Action, checkContextAccess, setContextAccess } from '../actions/context';
 
-export const checkAccessContextReducer = (initial: State) => createReducer<State, Action>(initial)
-  .handleAction(checkContextAccess.request, (state, action) => ({
-    ...state,
-    hasContextAccess: undefined,
-    status: [...state.status, Status.AccessCheck],
-    errors: removeError(state, action)
-  }))
-  .handleAction(checkContextAccess.success, (state) => ({
-    ...state,
-    hasContextAccess: true,
-    status: removeStatus(state, Status.AccessCheck),
-  }))
-  .handleAction(checkContextAccess.failure, (state, action) => ({
-    ...state,
-    hasContextAccess: false,
-    status: removeStatus(state, Status.AccessCheck),
-    errors: [...state.errors, action.payload],
-  }))
+export const checkAccessContextReducer = (initial: State) =>
+    createReducer<State, Action>(initial)
+        .handleAction(checkContextAccess.request, (state, action) => ({
+            ...state,
+            hasContextAccess: undefined,
+            status: [...state.status, Status.AccessCheck],
+            errors: removeError(state, action),
+        }))
+        .handleAction(checkContextAccess.success, (state) => ({
+            ...state,
+            hasContextAccess: true,
+            status: removeStatus(state, Status.AccessCheck),
+        }))
+        .handleAction(checkContextAccess.failure, (state, action) => ({
+            ...state,
+            hasContextAccess: false,
+            status: removeStatus(state, Status.AccessCheck),
+            errors: [...state.errors, action.payload],
+        }))
 
-  .handleAction(setContextAccess, (state, action) =>
-    state.hasContextAccess === action.payload ? state : { ...state, hasContextAccess: action.payload }
-  )
-
+        .handleAction(setContextAccess, (state, action) =>
+            state.hasContextAccess === action.payload
+                ? state
+                : { ...state, hasContextAccess: action.payload }
+        );
 
 export default checkAccessContextReducer;
