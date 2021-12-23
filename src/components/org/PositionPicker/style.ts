eda516c8b19738ace796f1787fcfd9505d5ee062
AbdullZahir <Abdullah.zahir@hotmail.com>
2021-12-23 10:18:09 +0100

    fix: updating styling for positionPicker component

---
+++ src/components/org/PositionPicker/style.ts
@@ -0,0 +1,18 @@
+import { makeStyles, createStyles } from '@equinor/fusion-react-styles';
+
+export const useStyles = makeStyles(
+    createStyles({
+        cardContainer: {
+            marginLeft: 'calc(var(--grid-unit) * 1px)',
+        },
+        positionName: {
+            fontSize: '14px',
+            lineHeight: '24px',
+        },
+        personName: {
+            fontSize: '14px',
+            lineHeight: '24px',
+        },
+    })
+);
+export default useStyles;
