805a1d9b01aecf7aa63cd7f9afd984fa55a78fce
AbdullZahir <Abdullah.zahir@hotmail.com>
2022-01-27 13:59:03 +0100

    merge with master into styling branch

--- src/components/general/Scrim/Scrim.style.ts
+++ src/components/general/Scrim/Scrim.style.ts
@@ -19,19 +19,3 @@ export const useStyles = makeStyles(
     })
 );
 export default useStyles;
-
-// .scrim {
-//     top: 0;
-//     bottom: 0;
-//     left: 0;
-//     right: 0;
-//     position: fixed;
-//     opacity: 0;
-//     transition: opacity 0.2s ease;
-//     background-color: rgba(0, 0, 0, 0.32);
-
-//     &.show {
-//         opacity: 1;
-//         pointer-events: all;
-//     }
-// }
