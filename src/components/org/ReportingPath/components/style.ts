08d74e1361c227d790cada6b134b35f5e73af862
AbdullZahir <Abdullah.zahir@hotmail.com>
2022-01-28 10:56:29 +0100

    merged master into styling branch

--- src/components/org/ReportingPath/components/style.ts
+++ src/components/org/ReportingPath/components/style.ts
@@ -1,26 +1,28 @@
 import { makeStyles, createStyles } from '@equinor/fusion-react-styles';
 export const useStyles = makeStyles(
-    createStyles({
-        link: {
-            stroke: '#dcdcdc', // EDS Design Token Interactive/Disabled/Border 🎨 Grey 20
-            strokeWidth: '2px',
-            fill: 'none',
-            shapeRendering: 'crispEdges',
-        },
-        isLinked: {
-            strokeDasharray: 5,
-            stroke: '#bebebe', // EDS Design Token Interactive/Disabled/Text 🎨 Grey 30
-        },
-        card: {
-            fill: 'none',
-        },
-        labelRect: {
-            fill: 'none',
-        },
-        linkedIndicator: {
-            fill: 'none',
-        },
-    }),
+    (theme) =>
+        createStyles({
+            link: {
+                stroke: theme.colors.interactive.disabled__border.getVariable('color'),
+                //'#dcdcdc', // EDS Design Token Interactive/Disabled/Border 🎨 Grey 20
+                strokeWidth: '2px',
+                fill: 'none',
+                shapeRendering: 'crispEdges',
+            },
+            isLinked: {
+                strokeDasharray: 5,
+                stroke: theme.colors.interactive.disabled__text.getVariable('color'),
+            },
+            card: {
+                fill: 'none',
+            },
+            labelRect: {
+                fill: 'none',
+            },
+            linkedIndicator: {
+                fill: 'none',
+            },
+        }),
     { name: 'fusion-components-ReportingPath' }
 );
 export default useStyles;
