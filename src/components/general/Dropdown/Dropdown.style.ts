bbe9dc39441eb21eba4b789c38a0a1c58ee70861
Abdullah Zahir Babar <62884467+AbdullZahir@users.noreply.github.com>
2022-01-31 15:59:42 +0100

    fix: Dropdown styling update (#1347)
    * fix: Dropdown styling update
    
    * fix:updated styling for Dropdown component
    
    * merge master into styling branch

---
+++ src/components/general/Dropdown/Dropdown.style.ts
@@ -0,0 +1,25 @@
+import { createStyles, makeStyles } from '@equinor/fusion-react-styles';
+
+export const useStyles = makeStyles(
+    createStyles({
+        dropdownContainer: {
+            position: 'absolute',
+            top: '100%',
+            minWidth: '100%',
+            marginTop: 'calc(var(--grid-unit) * 0.5px)',
+            pointerEvents: 'all',
+            background: 'white',
+            borderRadius: '4px',
+            overflow: 'auto',
+            minHeight: 'calc(var(--grid-unit) * 8px)',
+        },
+        justifyLeft: {
+            right: 0,
+        },
+        justifyRight: {
+            left: 0,
+        },
+    }),
+    { name: 'fusion-components-dropdown' }
+);
+export default useStyles;
