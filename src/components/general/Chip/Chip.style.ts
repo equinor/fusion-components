0b09edb4dd53cd67bd8edf03e3a9aab64570e1d3
Abdullah Zahir Babar <62884467+AbdullZahir@users.noreply.github.com>
2022-01-31 16:03:00 +0100

    Chip style update (#1352)
    * fix: Chip Style update
    
    * fix: Chip component style updated
    
    * fix: updated styling for Chip component

---
+++ src/components/general/Chip/Chip.style.ts
@@ -0,0 +1,62 @@
+import { makeStyles, createStyles } from '@equinor/fusion-react-styles';
+
+export const useStyles = makeStyles(
+    createStyles({
+        chipContainer: {
+            display: 'inline-block',
+
+            '&:hover': {
+                '& $title': {
+                    color: 'var(--chip-hover-color)',
+                },
+                '& $removeButton': {
+                    color: 'var(--chip-hover-color)',
+                },
+            },
+
+            '--chip-color': 'var(--color-primary)',
+            '--chip-background-color': 'var(--color-black-alt5)',
+            '--chip-hover-color': 'var(--color-primary-hover)',
+        },
+        chip: {
+            alignItems: 'center',
+            backgroundColor: 'var(--chip-background-color)',
+            borderRadius: '16px',
+            color: 'var(--chip-color)',
+            cursor: 'default',
+            display: 'inline-flex',
+            padding: 'calc(var(--grid-unit) * 1px) calc(var(--grid-unit) * 2px)',
+        },
+        removeButton: {
+            cursor: 'pointer',
+            display: 'flex',
+        },
+        title: {
+            margin: 0,
+            '& + $removeButton': {
+                paddingLeft: 'calc(var(--grid-unit) * 1px)',
+            },
+        },
+        secondary: {
+            '--chip-color': 'var(--color-secondary)',
+            '--chip-background-color': 'var(--color-secondary-alt3)',
+            '--chip-hover-color': 'var(--color-secondary)',
+        },
+        primary: {
+            '--chip-color': 'var(--color-primary)',
+            '--chip-background-color': 'var(--color-primary-alt4)',
+            '--chip-hover-color': 'var(--color-primary-hover)',
+        },
+        disabled: {
+            '--chip-color': 'var(--color-black-alt2)',
+            '--chip-background-color': 'var(--color-black-alt5)',
+            '--chip-hover-color': 'var(--color-black-alt2)',
+
+            '& $chip': {
+                cursor: 'not-allowed',
+            },
+        },
+    }),
+    { name: 'fusion-component-chip' }
+);
+export default useStyles;
---
+++ src/components/general/Chip/index.ts
@@ -0,0 +1,1 @@
+export { Chip, default } from './Chip';
