7dce26b7df7033c85afbba3b6bb9cbea00afec48
AbdullZahir <Abdullah.zahir@hotmail.com>
2022-03-07 15:23:10 +0100

    used equinor color token

--- src/components/org/InlineDepartmentCard/index.tsx
+++ src/components/org/InlineDepartmentCard/index.tsx
@@ -8,49 +8,50 @@ type InlineDepartmentCardProps = {
 };
 
 const useDepartmentCardStyles = makeStyles(
-    createStyles({
-        container: {
-            display: 'grid',
-            gridTemplateColumns: '2rem 1fr',
-            alignItems: 'center',
-            padding: ' 0.2rem 0 0.2rem 0',
-            borderRadius: '4px',
-            width: '100%',
-            height: '40px',
-            background: '#FFFFFF',
-        },
-        logo: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            width: '100%',
-            height: '65%',
-        },
-        textContainer: {
-            marginLeft: '0.25rem',
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-        },
-        department: {
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            width: '90%',
-            fontSize: '12px',
-            lineHeight: '1rem',
-            fontWeight: 500,
-        },
-        name: {
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            width: '90%',
-            fontSize: '10px',
-            lineHeight: '12px',
-        },
-    }),
+    (theme) =>
+        createStyles({
+            container: {
+                display: 'grid',
+                gridTemplateColumns: '2rem 1fr',
+                alignItems: 'center',
+                padding: ' 0.2rem 0 0.2rem 0',
+                borderRadius: '4px',
+                width: '100%',
+                height: '40px',
+                background: theme.colors.ui.background__light.getVariable('color'),
+            },
+            logo: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                width: '100%',
+                height: '65%',
+            },
+            textContainer: {
+                marginLeft: '0.25rem',
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+            },
+            department: {
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                width: '90%',
+                fontSize: '12px',
+                lineHeight: '1rem',
+                fontWeight: 500,
+            },
+            name: {
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                width: '90%',
+                fontSize: '10px',
+                lineHeight: '12px',
+            },
+        }),
     { name: 'fusion-components-inline-department-card' }
 );
 
