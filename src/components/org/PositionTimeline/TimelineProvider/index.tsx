7829c19499fd1797423c33078b118fe05d1fc741
Jardar Tøn <jardt@equinor.com>
2023-01-04 09:56:40 +0100

    formatting

--- src/components/org/PositionTimeline/TimelineProvider/index.tsx
+++ src/components/org/PositionTimeline/TimelineProvider/index.tsx
@@ -4,92 +4,92 @@ import { reducer } from './reducer';
 import { TimelineState, TimelineContext, TimelineProviderProps } from './types';
 
 const initialContext = (partial: Partial<TimelineState>): TimelineState => ({
-	position: null,
-	splits: [],
-	rotationGroups: {},
-	selected: [],
-	highlighted: [],
-	disabled: [],
-	mode: 'single',
-	previewDates: false,
-	selectedDate: new Date(),
-	topSliderMarkers: [],
-	bottomSliderMarkers: [],
-	...partial,
+    position: null,
+    splits: [],
+    rotationGroups: {},
+    selected: [],
+    highlighted: [],
+    disabled: [],
+    mode: 'single',
+    previewDates: false,
+    selectedDate: new Date(),
+    topSliderMarkers: [],
+    bottomSliderMarkers: [],
+    ...partial,
 });
 
 export const timelineContext = createContext<TimelineContext>({
-	state: initialContext({}),
-	dispatch: () => undefined,
+    state: initialContext({}),
+    dispatch: () => undefined,
 });
 
 export const TimelineProvider = (props: PropsWithChildren<TimelineProviderProps>) => {
-	const {
-		selectedPosition,
-		mode,
-		initialSplit,
-		initialDate,
-		previewDates,
-		onDateChange,
-		onSingleSelect,
-		onMultiSelect,
-		highlighted,
-		disabled,
-		PersonSlot,
-		InfoSlot,
-		ActionSlot,
-		children,
-	} = props;
+    const {
+        selectedPosition,
+        mode,
+        initialSplit,
+        initialDate,
+        previewDates,
+        onDateChange,
+        onSingleSelect,
+        onMultiSelect,
+        highlighted,
+        disabled,
+        PersonSlot,
+        InfoSlot,
+        ActionSlot,
+        children,
+    } = props;
 
-	const [state, dispatch] = useReducer(
-		reducer(
-			initialContext({
-				mode,
-				previewDates,
-				selected: initialSplit?.id ? [initialSplit.id] : [],
-			})
-		),
-		initialContext({ mode, previewDates, selected: initialSplit?.id ? [initialSplit.id] : [] })
-	);
+    const [state, dispatch] = useReducer(
+        reducer(
+            initialContext({
+                mode,
+                previewDates,
+                selected: initialSplit?.id ? [initialSplit.id] : [],
+            })
+        ),
+        initialContext({ mode, previewDates, selected: initialSplit?.id ? [initialSplit.id] : [] })
+    );
 
-	useEffect(() => {
-		if (selectedPosition.instances.length)
-			dispatch(actions.setPosition({ position: selectedPosition, initialDate }));
-		return () => dispatch(actions.reset());
-	}, [selectedPosition, initialDate]);
+    useEffect(() => {
+        if (selectedPosition.instances.length)
+            dispatch(actions.setPosition({ position: selectedPosition, initialDate }));
+        return () => dispatch(actions.reset());
+    }, [selectedPosition, initialDate]);
 
-	useEffect(() => {
-		if (!onDateChange) return;
-		onDateChange(state.selectedDate);
-	}, [state.selectedDate]);
+    useEffect(() => {
+        if (!onDateChange) return;
+        onDateChange(state.selectedDate);
+    }, [state.selectedDate]);
 
-	useEffect(() => {
-		if (!onSingleSelect) return;
-		state.selected.map((id) => onSingleSelect(id));
-	}, [state.selected]);
+    useEffect(() => {
+        if (!onSingleSelect) return;
+        state.selected.map((id) => onSingleSelect(id));
+    }, [state.selected]);
 
-	useEffect(() => {
-		if (!onMultiSelect) return;
-		onMultiSelect(state.selected);
-	}, [state.selected]);
+    useEffect(() => {
+        if (!onMultiSelect) return;
+        onMultiSelect(state.selected);
+    }, [state.selected]);
 
-	useEffect(() => {
-		if (!highlighted) return;
-		dispatch(actions.setHighlighted(highlighted));
-	}, [highlighted]);
+    useEffect(() => {
+        if (!highlighted) return;
+        dispatch(actions.setHighlighted(highlighted));
+    }, [highlighted]);
 
-	useEffect(() => {
-		if (!disabled) return;
-		dispatch(actions.setDisabled(disabled));
-	}, [disabled]);
+    useEffect(() => {
+        if (!disabled) return;
+        dispatch(actions.setDisabled(disabled));
+    }, [disabled]);
 
-	useEffect(() => {
-		dispatch(actions.setCustomSlots({ PersonSlot, InfoSlot, ActionSlot }));
-	}, [PersonSlot, ActionSlot, InfoSlot]);
+    useEffect(() => {
+        dispatch(actions.setCustomSlots({ PersonSlot, InfoSlot, ActionSlot }));
+    }, [PersonSlot, ActionSlot, InfoSlot]);
 
-	return (
-		<timelineContext.Provider value={{ state, dispatch }}>{children}</timelineContext.Provider>
-	);
+    return (
+        <timelineContext.Provider value={{ state, dispatch }}>{children}</timelineContext.Provider>
+    );
 };
 
 export default TimelineProvider;
