3c8f0ca97bd38a41ca6c34df552de5611efd7f50
Jardar Tøn <43233498+jardt@users.noreply.github.com>
2023-01-20 14:32:06 +0100

    fix: sorting and hightlight fix for timeline preview (#1584)

--- src/components/org/PositionTimeline/components/RotationSequence/utils.ts
+++ src/components/org/PositionTimeline/components/RotationSequence/utils.ts
@@ -1,5 +1,8 @@
 import { RotationGroups } from '../../model';
 
 export const getSortedRotationKeys = (rotationGroups: RotationGroups): string[] => {
-    return Object.keys(rotationGroups).sort((a, b) => a.localeCompare(b));
+    return Object.keys(rotationGroups).sort((a, b) => {
+        if (isNaN(parseInt(a)) || isNaN(parseInt(b))) return a.localeCompare(b);
+        return parseInt(a) - parseInt(b);
+    });
 };
