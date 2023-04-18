91e6a61089d88b401f7a649db450fc0491525bf4
Øyvind Eikeland <oyvind@eikeland.me>
2023-04-18 16:17:45 +0200

    fix(pbi): typo in pbi errorboundary

--- src/components/data/PowerBI/Report/components/error/process-action-error.ts
+++ src/components/data/PowerBI/Report/components/error/process-action-error.ts
@@ -6,32 +6,33 @@ import { ErrrorProperties } from '.';
 import { processError } from './process-error';
 
 export const processActionError = ({ error, action }: ApiError): ErrrorProperties => {
-  const code = Number(error.statusCode);
-  const options = processError(error);
-  switch (action.type) {
-      case getType(actions.fetchEmbedInfo.request): {
-          const title = 'Sorry we could load the Power BI embedding information for the report';
-          switch (code) {
-              case 403:
-                  return {
-                      ...options,
-                      title,
-                      type: 'accessDenied',
-                      message: 'You do not have access to view the report',
-                  };
-              case 404:
-                  return {
-                      ...options,
-                      title,
-                      type: 'notFound',
-                      message: 'The report does not exist',
-                  };
-              default:
-                  return { ...options, title };
-          }
-      }
-  }
-  return options;
+    const code = Number(error.statusCode);
+    const options = processError(error);
+    switch (action.type) {
+        case getType(actions.fetchEmbedInfo.request): {
+            const title =
+                'Sorry we could not load the Power BI embedding information for the report';
+            switch (code) {
+                case 403:
+                    return {
+                        ...options,
+                        title,
+                        type: 'accessDenied',
+                        message: 'You do not have access to view the report',
+                    };
+                case 404:
+                    return {
+                        ...options,
+                        title,
+                        type: 'notFound',
+                        message: 'The report does not exist',
+                    };
+                default:
+                    return { ...options, title };
+            }
+        }
+    }
+    return options;
 };
 
 export default processActionError;
